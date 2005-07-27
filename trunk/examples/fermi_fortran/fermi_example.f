020321c5b89bd602dbaa19406bd973556c0c4ac2
dkearney <dkearney@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2005-07-27 21:37:14 +0000

    adding fermi_fortran as an example of a working app

---
+++ trunk/examples/fermi_fortran/fermi_example.f
@@ -0,0 +1,71 @@
+c   file fermi_example.f
+
+        program fermi_example
+            IMPLICIT NONE
+
+            integer rp_lib
+            real*8 rp_lib_get_double
+            
+            CHARACTER*100 inFile, outFile
+            character*100 T_path, Ef_path, outPath
+            character*40 xy
+            integer handle
+            real*8 T,Ef,kT,Emin,Emax,dE,f,E,kTconstExp
+
+            ! inFile = "driver.xml"
+            call getarg(1,inFile)
+            outFile = "run.xml"
+
+            handle = 0
+
+            handle = rp_lib(inFile)
+
+            T_path = "input.(ambient).(temperature).current"
+            Ef_path = "input.number(Ef).default"
+
+            T = rp_lib_get_double(handle,T_path)
+            Ef = rp_lib_get_double(handle,Ef_path)
+
+
+c            kT = 8.61734e-5 * T
+            kTconstExp = (10.0)**(-5.0)
+            kT = (8.61734*kTconstExp) * T 
+
+            Emin = (Ef - (10*kT))
+            Emax = (Ef + (10*kT))
+
+            dE = (0.005)*(Emax - Emin)
+
+            outPath = "output.curve(f12).xaxis.label"
+            call rp_lib_put_str(handle,outPath,"f12 x label",1)
+            outPath = "output.curve(f12).yaxis.label"
+            call rp_lib_put_str(handle,outPath,"f12 y label",1)
+
+            outPath = "output.curve(f12).component.xy"
+
+            do 15,E = Emin,Emax,dE
+                f = 1.0/(1.0+exp((E-Ef)/kT))
+                write(xy,'(E20.12,F13.9,A)')f,E,char(10)
+                call rp_lib_put_str(handle,outPath,xy,1)
+ 15         continue
+
+            outPath = "output.curve(f13).xaxis.label"
+            call rp_lib_put_str(handle,outPath,"f13 x label",1)
+            outPath = "output.curve(f13).yaxis.label"
+            call rp_lib_put_str(handle,outPath,"f13 y label",1)
+
+            outPath = "output.curve(f13).component.xy"
+
+            do 16,E = Emin,Emax,dE
+                f = -1.0/(1.0+exp((E-Ef)/kT))
+                write(xy,'(E20.12,F13.9,A)')f,E,char(10)
+                call rp_lib_put_str(handle,outPath,xy,1)
+ 16         continue
+
+            
+            call rp_lib_write_xml(handle, outFile)
+            call rp_quit()
+
+            print *,"=RAPPTURE-RUN=>"//outFile
+
+        end program fermi_example
