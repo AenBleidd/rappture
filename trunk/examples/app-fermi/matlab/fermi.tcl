de08b26840025f5609f4c38eece0d5e0432907d2
mmc <mmc@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2005-09-29 18:22:25 +0000

    Changed the example to tell a better story in the tutorial. Ef should be the first parameter, because it's simpler. Outputs f and E should appear in that order, to be consistent with later examples.

--- trunk/examples/app-fermi/matlab/fermi.tcl
+++ trunk/examples/app-fermi/matlab/fermi.tcl
@@ -13,14 +13,14 @@ package require Rappture
 # open the XML file containing the run parameters
 set driver [Rappture::library [lindex $argv 0]]
 
-set T [$driver get input.(temperature).current]
-set T [Rappture::Units::convert $T -to K -units off]
 set Ef [$driver get input.(Ef).current]
 set Ef [Rappture::Units::convert $Ef -to eV -units off]
+set T [$driver get input.(temperature).current]
+set T [Rappture::Units::convert $T -to K -units off]
 
 set fid [open "indeck" w]
-puts $fid $T
 puts $fid $Ef
+puts $fid $T
 close $fid
 
 set status [catch {Rappture::exec octave --silent fermi.m < indeck} out]
@@ -33,7 +33,7 @@ if {$status == 0} {
 
     # skip over the first 4 header lines
     foreach line [lrange [split $info \n] 5 end] {
-        if {[scan $line {%g %g} E f] == 2} {
+        if {[scan $line {%g %g} f E] == 2} {
             $driver put -append yes output.curve(f12).component.xy "$f $E\n"
         }
     }
