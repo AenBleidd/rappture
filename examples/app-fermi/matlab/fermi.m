5e64b76688e6ec14a9e1e493d2d92d0f1c845308
mmc <mmc@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2005-09-29 18:22:25 +0000

    Changed the example to tell a better story in the tutorial. Ef should be the first parameter, because it's simpler. Outputs f and E should appear in that order, to be consistent with later examples.

--- examples/app-fermi/matlab/fermi.m
+++ examples/app-fermi/matlab/fermi.m
@@ -8,11 +8,11 @@
 %  Copyright (c) 2004-2005
 %  Purdue Research Foundation, West Lafayette, IN
 % ======================================================================
-disp('Enter the temperature (K):');
-T = input(' T = ');
-
 disp('Enter the Fermi level (eV):');
 Ef = input(' Ef = ');
+
+disp('Enter the temperature (K):');
+T = input(' T = ');
 
 kT = 8.61734e-5 * T;
 Emin = Ef - 10*kT;
@@ -23,9 +23,9 @@ f = 1.0 ./ (1.0 + exp((E - Ef)/kT));
 
 fid = fopen('out.dat','w');
 fprintf(fid,'FERMI-DIRAC FUNCTION F1/2\n\n');
-fprintf(fid,'Energy (eV)   f1/2\n');
+fprintf(fid,'f1/2          Energy (eV)\n');
 fprintf(fid,'------------  ------------\n');
 for j=1:200
-  fprintf(fid,'%12g  %12g\n', E(j), f(j));
+  fprintf(fid,'%12g  %12g\n', f(j), E(j));
 end
 fclose(fid);
