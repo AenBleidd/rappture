510f8e85c89633c10ee09bf0e43c5d700f416a94
mmc <mmc@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2005-11-05 07:08:21 +0000

    - Added a new <image> which can be used on both the input   and the output side.
    - Added examples to the zoo for images and tables (energy viewer).

---
+++ trunk/examples/zoo/table/energies.tcl
@@ -0,0 +1,35 @@
+# ----------------------------------------------------------------------
+#  EXAMPLE: Rappture <energies> elements
+# ======================================================================
+#  AUTHOR:  Michael McLennan, Purdue University
+#  Copyright (c) 2004-2005  Purdue Research Foundation
+#
+#  See the file "license.terms" for information on usage and
+#  redistribution of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+# ======================================================================
+package require Rappture
+
+# open the XML file containing the run parameters
+set driver [Rappture::library [lindex $argv 0]]
+
+set L [$driver get input.number(L).current]
+set L [Rappture::Units::convert $L -to "m" -units off]
+
+set emass [$driver get input.number(emass).current]
+set m [expr {$emass*9.11e-31}]  ;# kg
+
+set h 4.13566743e-15  ;# in eVs
+set J2eV 6.241506363e17
+
+set nhomo [expr {round(rand()*19+1)}]
+
+for {set n 1} {$n <= 20} {incr n} {
+    set E [expr {$n*$n*$h*$h/(8.0*$m*$L*$L*$J2eV)}]  ;# in eV
+    set label [expr {($n == $nhomo) ? "HOMO" : $n}]
+
+    $driver put -append yes output.table.data [format "%s %.3g\n" $label $E]
+}
+
+# save the updated XML describing the run...
+Rappture::result $driver
+exit 0
---
+++ trunk/gui/scripts/image.tcl
@@ -0,0 +1,62 @@
+# ----------------------------------------------------------------------
+#  COMPONENT: image - represents a picture image
+#
+#  This object represents a Tk image.  It is convenient to have it
+#  expressed as an Itcl object, so it can be managed just like a
+#  curve, table, etc.
+# ======================================================================
+#  AUTHOR:  Michael McLennan, Purdue University
+#  Copyright (c) 2004-2005  Purdue Research Foundation
+#
+#  See the file "license.terms" for information on usage and
+#  redistribution of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+# ======================================================================
+package require Itcl
+package require BLT
+
+namespace eval Rappture { # forward declaration }
+
+itcl::class Rappture::Image {
+    constructor {xmlobj path} { # defined below }
+    destructor { # defined below }
+
+    public method tkimage {} { return $_image }
+    public method hints {{keyword ""}}
+
+    private variable _xmlobj ""  ;# ref to lib obj with curve data
+    private variable _image ""   ;# underlying image data
+}
+
+# ----------------------------------------------------------------------
+# CONSTRUCTOR
+# ----------------------------------------------------------------------
+itcl::body Rappture::Image::constructor {xmlobj path} {
+    if {![Rappture::library isvalid $xmlobj]} {
+        error "bad value \"$xmlobj\": should be LibraryObj"
+    }
+    set _xmlobj $xmlobj
+    set data [string trim [$xmlobj get $path.current.data]]
+    if {[string length $data] == 0} {
+        set _image [image create photo]
+    } else {
+        set _image [image create photo -data $data]
+    }
+}
+
+# ----------------------------------------------------------------------
+# DESTRUCTOR
+# ----------------------------------------------------------------------
+itcl::body Rappture::Image::destructor {} {
+    image delete $_image
+}
+
+# ----------------------------------------------------------------------
+# USAGE: hints ?<keyword>?
+#
+# Returns a list of key/value pairs for various hints about showing
+# this image.  If a particular <keyword> is specified, then it returns
+# the hint for that <keyword>, if it exists.
+# ----------------------------------------------------------------------
+itcl::body Rappture::Image::hints {{keyword ""}} {
+    return ""
+}
