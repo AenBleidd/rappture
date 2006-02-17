7eb24793b62a9b366a50a432163b861da288ec95
cxsong <cxsong@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2006-02-17 04:38:05 +0000

    updates

--- src/mesh/grid1d_reg.h
+++ src/mesh/grid1d_reg.h
@@ -6,47 +6,46 @@
 // defined by  a starting point and delta increment.
 // 
 //
-
 #include <vector>
-#include "rp_defs.h"
+#include "util.h"
 
 class RpGrid1d_reg {
 public:
 	// constructors
 	RpGrid1d_reg() :
-		_npoints(0),
-		_start(0.0),
-		_delta(0.0)
+		m_npoints(0),
+		m_start(0.0),
+		m_delta(0.0)
 	{ };
 
 	// does not expand to array
 	RpGrid1d_reg(int npts, double delta, double st) :
-		_npoints(npts),
-		_start(st),
-		_delta(delta)
+		m_npoints(npts),
+		m_start(st),
+		m_delta(delta)
 	{ };
 
 	RpGrid1d_reg(int npts, double delta):
-		_npoints(npts),
-		_start(0.0),
-		_delta(delta)
+		m_npoints(npts),
+		m_start(0.0),
+		m_delta(delta)
 	{ };
 
 	// copy constructor
 	//RpGrid1d_reg(const RpGrid1d_reg& c);
 
-	double delta() { return _delta; };
-	void delta(double d) { _delta = d; };
-	double start() { return _start; };
-	void start(double s) { _start = s; };
-	int points() { return _npoints; };
-	void points(int n) { _npoints = n; };
+	double delta() { return m_delta; };
+	void delta(double d) { m_delta = d; };
+	double start() { return m_start; };
+	void start(double s) { m_start = s; };
+	int points() { return m_npoints; };
+	void points(int n) { m_npoints = n; };
 
 	// construct the object with byte stream containing number of points and delta value
 	RpGrid1d_reg(const char* buf);
 
 	// return number of points in grid
-	int npoints() { return _npoints; }
+	int npoints() { return m_npoints; }
 
 	// serialize object 
 	char * serialize();
@@ -63,10 +62,10 @@ public:
 	//virtual int xmlGet() { };
 
 private:
-	int _npoints;
-	double _start;
-	double _delta;
-	vector<double> _data;
+	int m_npoints;
+	double m_start;
+	double m_delta;
+	vector<double> m_data;
 };
 
 #endif
