9d2af16259cc632dd5ee1b379654fb818f1c1ac5
cxsong <cxsong@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2006-02-17 16:30:27 +0000

    added sets

--- src/mesh/node.h
+++ src/mesh/node.h
@@ -9,14 +9,22 @@
 class RpNode {
 public:
         // constructors (default to 2d node)
-        RpNode(int dim=2):
-		m_dim(dim)
+        RpNode(int dim=2, const char* id=NULL):
+		m_dim(dim),
+		m_id(id)
 	{ 
 		m_node.resize(dim);
 	}
 
 	// assign value to coordinate[index]
 	RP_ERROR set(int val, int index);
+	RP_ERROR get(int& val, int index);
+
+	void setNode2d(int x, int y, const char* id);
+	void setNode3d(int x, int y, int z, const char* id);
+
+	const char* id() { return m_id.c_str(); };
+	void id(const char* id) { m_id = id; };
 
 	char* serialize();
 	void serialize(const char* buf, int buflen);
@@ -29,6 +37,7 @@ public:
 private:
 	vector<int> m_node;
 	int m_dim;
+	std::string m_id;
 };
 
 #endif
