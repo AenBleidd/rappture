75de4c0749d72a4ec26a0936691a92dd82c79fd8
cxsong <cxsong@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2006-02-20 16:21:24 +0000

    changed data representation

--- trunk/src/mesh/element.h
+++ trunk/src/mesh/element.h
@@ -6,30 +6,68 @@
 #include "node.h"
 #include "util.h"
 
+//
+// RpElement is defined as a list of nodes (by id) connected with each other
+// in a mesh.
+//
+// Example:
+// 	int nodeIDs[8];
+// 	RpElement elements[10];
+//
+//      // set ids for all elements
+// 	for (int i=0; i<10; i++)
+// 		elements[i].id(i);
+//
+//	// create node list for element 0
+// 	nodeIDs[0] = 0;
+// 	nodeIDs[1] = 1;
+// 	nodeIDs[2] = 5;
+// 	nodeIDs[3] = 6;
+// 	nodeIDs[4] = 25;
+// 	nodeIDs[5] = 26;
+// 	nodeIDs[6] = 30;
+// 	nodeIDs[7] = 31;
+// 	element[0].addNodes(nodeIDs, 8);
+//
+// 	nodeIDs[0] = 1;
+// 	nodeIDs[1] = 2;
+// 	nodeIDs[2] = 6;
+// 	nodeIDs[3] = 7;
+// 	nodeIDs[4] = 26;
+// 	nodeIDs[5] = 27;
+// 	nodeIDs[6] = 31;
+// 	nodeIDs[7] = 32;
+// 	element[1].addNodes(nodeIDs, 8);
+//
+//      ... 
+//
+//
 
 class RpElement {
 public:
         // constructors
-        RpElement() {  };
-
-        RpElement(int size, int id):
-		m_size(size), 
-		m_id(id)
-	{  
-		m_nodes.resize(size);
+        RpElement()
+	{
+		// default
+		m_nodes(8);
 	};
 
-	// add a node to element
-	void addNode(RpNode& node);
+        RpElement(int numNodes, int id)
+	{
+		m_id = id;
+		if (m_nodes.capacity() < numNodes)
+			m_nodes.resize(numNodes);
+	};
 
-	// add node name - may remove this later
+	// instantiate from byte stream:
+	// id(int), numNodes(int), list of ids(int *)
+        RpElement(const char* buf);
+
+	// add a node to element
 	void addNode(int nodeName);
 
-	// add all nodes to element
-	void setNodes(RpNode* list, int numNodes, int id);
-
-	// set node names
-	RP_ERROR set(int* nodes, int numNodes, int id);
+	// add all the nodes to element
+	RP_ERROR addNodes(const int* nodes, int numNodes);
 	
 	// get node id's
 	RP_ERROR getNodeIdList(int* list, int len);
@@ -50,10 +88,8 @@ public:
 	// void xmlGet();
 
 private:
-	int m_size;
 	int m_id;
-	vector<int> m_nodes;
-	vector<RpNode> m_list;
+	vector<int> m_nodes; // list of node ids
 };
 
 #endif
