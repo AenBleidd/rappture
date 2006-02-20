edc77b4558eabce68d92874767e19eaece4f4a52
cxsong <cxsong@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2006-02-20 21:43:09 +0000

    changed from node.xx

---
+++ trunk/src/mesh/node3d.h
@@ -0,0 +1,54 @@
+#ifndef __RP_NODE3D_H__
+#define __RP_NODE3D_H__
+
+#include <iostream>
+#include <vector>
+#include "util.h"
+
+//
+// define a 3-d node (x, y, z)
+//
+class RpNode3d {
+public:
+        // constructors
+        RpNode3d() { };
+
+        RpNode3d(int id): m_id(id)
+	{ };
+
+        RpNode3d(int id, int x, int y, int z):
+		m_id(id),
+		m_x(x),
+		m_y(y),
+		m_z(z)
+	{ };
+
+	// assign value to coordinate[index]
+	void set(int x, int y, int z) { m_x = x; m_y = y; m_z =z; };
+	void set(int* val) { m_x = val[0]; m_y = val[1]; m_z = val[2]; };
+
+	// get node coordinates
+	void get(int& xval, int& yval, int& zval) 
+	{ xval = m_x; yval = m_y; zval = m_z; };
+
+	void get(int * val) { val[0] = m_x; val[1] = m_y; val[2] = m_z; };
+
+	int id() { return m_id; };
+	void id(int id) { m_id = id; };
+
+	char* serialize();
+	RP_ERROR serialize(char* buf, int buflen);
+	RP_ERROR deserialize(const char* buf);
+
+        void print();
+
+        virtual ~RpNode3d() { };
+
+private:
+	int m_id;
+	int m_x;
+	int m_y;
+	int m_z;
+};
+
+#endif
