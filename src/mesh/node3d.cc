e764981724b8fd56916308d93794ee6da0ec31e2
cxsong <cxsong@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2006-02-20 21:43:09 +0000

    changed from node.xx

---
+++ src/mesh/node3d.cc
@@ -0,0 +1,75 @@
+#include "node3d.h"
+
+//
+// - serialize node (x y z) 
+// - store byte stream in buf
+//
+// Return: 
+// 	NULL: fail to allocate memory
+// 	valid pointer: point to buffer holding the byte stream
+//
+char* 
+RpNode3d::serialize()
+{
+	int len = 3 * sizeof(int);
+
+	char * buf = new char[len];
+	serialize(buf, len);
+
+	return buf;
+}
+
+//
+// actual function that does the serialization
+//
+// Input:
+// 	buf: pointer to valid memory
+// 	len: number of bytes in buf
+// Return:
+// 	error code for null pointer (defined in "util.h")
+// 	success
+//
+RP_ERROR 
+RpNode3d::serialize(char* buf, int buflen)
+{
+	if (buf == NULL || buflen < 3*((signed)sizeof(int))) {
+		RpAppendErr("RpElement::deserialize: null buffer");
+		RpPrintErr();
+		return RP_ERR_NULL_PTR;
+	}
+
+	char * ptr = buf;
+	memcpy((void*)ptr, (void *)&m_x, sizeof(int));
+	ptr += sizeof(int);
+	memcpy((void*)ptr, (void *)&m_y, sizeof(int));
+	ptr += sizeof(int);
+	memcpy((void*)ptr, (void *)&m_z, sizeof(int));
+
+	return RP_SUCCESS;
+}
+
+RP_ERROR
+RpNode3d::deserialize(const char* buf)
+{
+	if (buf == NULL) {
+		RpAppendErr("RpElement::deserialize: null buffer");
+		RpPrintErr();
+		return RP_ERR_NULL_PTR;
+	}
+
+	char * ptr = (char*)buf;
+	memcpy((void*)&m_x, (void *)ptr, sizeof(int));
+	ptr += sizeof(int);
+	memcpy((void*)&m_y, (void *)ptr, sizeof(int));
+	ptr += sizeof(int);
+	memcpy((void*)&m_z, (void *)ptr, sizeof(int));
+	
+	return RP_SUCCESS;
+}
+
+void 
+RpNode3d::print()
+{
+	printf("<node id=\"%d\"> %d %d %d</node>\n",m_id,m_x,m_y,m_z);
+}
+
