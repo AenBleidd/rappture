2fad5bb727c3c75ab380ca6c4805ed7e7950cb8e
dkearney <dkearney@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2006-02-22 19:16:19 +0000

    reorganized tcl directory

---
+++ trunk/src/tcl/src/Rappture_Init.cc
@@ -0,0 +1,35 @@
+/*
+ * ----------------------------------------------------------------------
+ *  Rappture_Init
+ *
+ *  This file contains the function that initializes all of the various
+ *  extensions in this package.
+ * ======================================================================
+ *  AUTHOR:  Derrick Kearney, Purdue University
+ *  Copyright (c) 2004-2006  Purdue Research Foundation
+ *
+ *  See the file "license.terms" for information on usage and
+ *  redistribution of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+ * ======================================================================
+ */
+#include <tcl.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int Rappture_Init _ANSI_ARGS_((Tcl_Interp *interp));
+int Rappturelibrary_Init _ANSI_ARGS_((Tcl_Interp *interp));
+
+#ifdef __cplusplus
+}
+#endif
+
+int
+Rappture_Init( Tcl_Interp * interp)
+{
+    if (Rappturelibrary_Init(interp) != TCL_OK) {
+        return TCL_ERROR;
+    }
+    return TCL_OK;
+}
