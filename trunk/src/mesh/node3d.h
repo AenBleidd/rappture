29fa7a3b89284b886b35c91fd3516517395be2a3
cxsong <cxsong@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2006-02-23 16:26:09 +0000

    various changes made 2/22

--- trunk/src/mesh/node3d.h
+++ trunk/src/mesh/node3d.h
@@ -23,9 +23,14 @@ public:
 		m_z(z)
 	{ };
 
+	void operator=(const int* val)
+		{ m_x = val[0]; m_y = val[1]; m_z = val[2]; };
+	
 	// assign value to coordinate[index]
-	void set(int x, int y, int z) { m_x = x; m_y = y; m_z =z; };
-	void set(int* val) { m_x = val[0]; m_y = val[1]; m_z = val[2]; };
+	void set(int x, int y, int z) 
+		{ m_x = x; m_y = y; m_z =z; };
+	void set(int* val) 
+		{ m_x = val[0]; m_y = val[1]; m_z = val[2]; };
 
 	// get node coordinates
 	void get(int& xval, int& yval, int& zval) 
