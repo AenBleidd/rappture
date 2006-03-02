e2065c5774b3f3083340b545d160822908addd7c
cxsong <cxsong@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2006-03-02 17:07:05 +0000

    modified serialization/deserializaiton functions, added xmlString, print, capacity, etc.

--- src/mesh/grid1d.h
+++ src/mesh/grid1d.h
@@ -16,18 +16,36 @@ public:
 	RpGrid1d(double* data, int size); // makes a copy of data
 	RpGrid1d(const char* buf); // instantiate with byte stream
 
+	// add all points to grid
+	RP_ERROR addAllPoints(double* val, int nitems);
+
+	// add one point to grid
 	void addPoint(double val);
 
 	// return number of points in grid
 	int numPoints() { return m_data.size(); }
 
+	// max num points that can be stored
+	int capacity() { return m_data.capacity(); }
+
 	// change the size of the grid after grid is constructed
 	void resize(int npoints) { m_data.resize(npoints); }
 
 	// serialize data 
-	char * serialize(RP_ENCODE_ALG eflag=RP_NO_ENCODING, 
+	// If no input parameters are provided, byte stream will be binary
+	// without compression.
+	//
+	char * serialize(int& nbytes, RP_ENCODE_ALG eflag=RP_NO_ENCODING, 
 			 RP_COMPRESSION cflag=RP_NO_COMPRESSION);
-	int deserialize(const char* buf);
+
+	RP_ERROR serialize(char * buf, int nbytes,
+			RP_ENCODE_ALG eflag=RP_NO_ENCODING, 
+			RP_COMPRESSION cflag=RP_NO_COMPRESSION);
+
+	RP_ERROR deserialize(const char* buf);
+
+	void xmlString(std::string& str);
+	void print();
 
 	// destructor
 	virtual ~RpGrid1d() { };
