b83854b2c40ce5849e1ac590663e349afcd5e89d
qiaow <qiaow@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2006-03-02 04:08:27 +0000

    Added config.h. It contains global configuration variables. 	One configuration variable is added: "NV40" 			-- Use 6 series hardware (NV40). Comment its define will enable compatibility to 5xxx series cards (NV30). NV30 				 only supports  8-bit per component textures with linear filtering. Whereas NV40 supports 16bit per 				 component texture with linear filtering, thus can store negative values.

---
+++ trunk/gui/vizservers/nanovis/config.h
@@ -0,0 +1,17 @@
+/*
+ * ----------------------------------------------------------------------
+ * Nanovis: Visualization of Nanoelectronics Data
+ *
+ * ======================================================================
+ *  AUTHOR:  Wei Qiao <qiaow@purdue.edu>
+ *           Purdue Rendering and Perceptualization Lab (PURPL)
+ *
+ *  Copyright (c) 2004-2006  Purdue Research Foundation
+ *
+ *  See the file "license.terms" for information on usage and
+ *  redistribution of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+ * ======================================================================
+ */
+
+
+//#define NV40 	//Uncomment if using 6 series card. By default we assume older card the 5xxx series
