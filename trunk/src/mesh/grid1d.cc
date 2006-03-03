ebdf8e926fb906f888c4f0d78a8ebe5fae8e255f
cxsong <cxsong@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2006-03-03 15:46:00 +0000

    modified serialize/deserialize to handle big endian machines

--- trunk/src/mesh/grid1d.cc
+++ trunk/src/mesh/grid1d.cc
@@ -3,6 +3,7 @@
 //
 
 #include "grid1d.h"
+#include "byte_order.h"
 
 
 // constructor
@@ -24,7 +25,7 @@ RpGrid1d::RpGrid1d(const char * buf)
 //
 // instantiate grid with a 1d array of doubles and number of items
 //
-RpGrid1d::RpGrid1d(double* val, int nitems)
+RpGrid1d::RpGrid1d(DataValType* val, int nitems)
 {
 	int sz = m_data.size();
 
@@ -37,14 +38,14 @@ RpGrid1d::RpGrid1d(double* val, int nitems)
 	// copy array val into m_data
 	sz = nitems;
 	void* ptr = &(m_data[0]);
-	memcpy(ptr, (void*)val, sizeof(double)*sz);
+	memcpy(ptr, (void*)val, sizeof(DataValType)*sz);
 }
 
 //
 // Add all points to grid1d object at once
 //
 RP_ERROR 
-RpGrid1d::addAllPoints(double* val, int nitems)
+RpGrid1d::addAllPoints(DataValType* val, int nitems)
 {
 	if (val == NULL)
 		return RP_ERR_NULL_PTR;
@@ -61,7 +62,7 @@ RpGrid1d::addAllPoints(double* val, int nitems)
 //
 // add one point to grid1d
 //
-void RpGrid1d::addPoint(double val)
+void RpGrid1d::addPoint(DataValType val)
 {
 	m_data.push_back(val);
 }
@@ -79,7 +80,7 @@ RpGrid1d::serialize(int& nb, RP_ENCODE_ALG encodeFlag,
 				RP_COMPRESSION compressFlag)
 {
 	int npts = m_data.size(); 
-	int nbytes = npts*sizeof(double) + sizeof(int) + 2;
+	int nbytes = npts*sizeof(DataValType) + sizeof(int) + 2;
 	char* buf;
 
 	// total length = tagEncode + tagCompress + num + array data 
@@ -101,7 +102,7 @@ RpGrid1d::serialize(char* buf, int nbytes, RP_ENCODE_ALG encodeFlag,
 {
 	int npts = m_data.size();
 
-	if (buf == NULL || (unsigned)nbytes < (npts*sizeof(double)+sizeof(int)+2)) {
+	if (buf == NULL || (unsigned)nbytes < (npts*sizeof(DataValType)+sizeof(int)+2)) {
                 RpAppendErr("RpGrid1d::serialize: invalid buffer");
 		RpPrintErr();
 		return RP_ERR_INVALID_ARRAY;
@@ -133,9 +134,18 @@ RpGrid1d::serialize(char* buf, int nbytes, RP_ENCODE_ALG encodeFlag,
 	// compress()
 
 	// write to stream buffer
-	memcpy((void*)ptr, (void*)&npts, sizeof(int));
+	//memcpy((void*)ptr, (void*)&npts, sizeof(int));
+	
+	// copy int to byte stream in LE byte order
+	int* iptr = (int*)ptr;
+	ByteOrder<int>::OrderCopy(&npts, iptr);
 	ptr += sizeof(int);
-	memcpy((void*)ptr, (void*)&(m_data[0]), npts*sizeof(double));
+
+	// copy data to byte stream in LE byte order
+	//memcpy((void*)ptr, (void*)&(m_data[0]), npts*sizeof(DataValType));
+	
+	DataValType* dptr = (DataValType*)ptr;
+	ByteOrder<DataValType>::OrderCopyArray(&(m_data[0]), dptr, npts);
 
 	return RP_SUCCESS;
 }
@@ -154,32 +164,37 @@ RpGrid1d::deserialize(const char* buf)
 	// TODO: handle encoding, decompression
 	
 	buf += 2; // skip 1st 2 bytes
+	int * iptr = (int*)buf;
 
 	// read number of points
-	memcpy((void*)&npts, (void*)buf, sizeof(int));
+	//memcpy((void*)&npts, (void*)buf, sizeof(int));
+
+	// copy int in buf to nptrs
+	// use ByteOrder::Copy to swap bytes if on a big endian machine
+	ByteOrder<int>::OrderCopy(iptr, &npts);
 	buf += sizeof(int);
 
-	m_data.resize(npts); // set the array to be the right size
-	memcpy((void*)&(m_data[0]), (void*)buf, npts*sizeof(double));
+	// set the array to be the right size
+	if (m_data.size() < (unsigned)npts)
+		m_data.resize(npts);
 
-	/* TODO
-	if (ByteOrder::IsBigEndian()) {
-		for (int i=0; i<npts; i++)
-		{
-			// flip each val
-		}
-	} */
+	// straight copy
+	//memcpy((void*)&(m_data[0]), (void*)buf, npts*sizeof(DataValType));
+	
+	// copy points array - use ByteOrder copy
+	DataValType* dptr = (DataValType*)buf;
+	ByteOrder<DataValType>::OrderCopyArray(dptr, &(m_data[0]), npts);
 
 	return RP_SUCCESS;
 }
 
-double*
+DataValType*
 RpGrid1d::data()
 {
 	int npts = numPoints();
 
-	double* xy;
-	if ( (xy = new double(npts)) == NULL) {
+	DataValType* xy;
+	if ( (xy = new DataValType(npts)) == NULL) {
                 RpAppendErr("RpGrid1d::data: mem alloc failed");
 		RpPrintErr();
 		return xy;
