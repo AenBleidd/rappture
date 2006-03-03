de13b02fa87913618ebfc0fc9560539ed28f6dc6
cxsong <cxsong@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2006-03-03 15:37:18 +0000

    added copy array functions

--- src/mesh/byte_order.h
+++ src/mesh/byte_order.h
@@ -1,6 +1,7 @@
 #ifndef __RP_BYTEORDER_H__
 #define __RP_BYTEORDER_H__
 
+//#include <stdio.h>
 #include <string.h>
 
 template<class ValType> class ByteOrder {
@@ -18,10 +19,10 @@ public:
 	// copy value from src to dst adjusting for endianess
 	// If the machine calling this function uses big endian byte order,
 	// bytes are flipped.
-	static void OrderCopy(const ValType& src_val, ValType& dst_val)
+	static void OrderCopy(const ValType* src_val, ValType* dst_val)
 	{
-		unsigned char* src = (unsigned char *)&src_val;
-		unsigned char* dst = (unsigned char *)&dst_val;
+		unsigned char* src = (unsigned char *)src_val;
+		unsigned char* dst = (unsigned char *)dst_val;
 	
 		// our software will use little endian
 		// swap bytes if reading on a big endian machine
@@ -50,17 +51,30 @@ public:
 			// LE: straight copy 
 			unsigned int i;
 			for (i=0; i<sizeof(ValType); i++) 
-				memcpy((void*)&dst, (void*)&src, sizeof(ValType));
+				memcpy((void*)dst, (void*)src, sizeof(ValType));
 		}
 	};
 
+	// copy an array of ValType elements
+	static void OrderCopyArray(const ValType* src_val, ValType* dst_val, int nitems)
+	{
+		for (int i=0; i < nitems; i++)
+			OrderCopy(&(src_val[i]), &(dst_val[i]));
+	};
+
 	// swap bytes in place, src altered for BE machine.
-	static void Flip(ValType& src_val)
+	static void Flip(ValType* src_val)
 	{
 		ValType val;
-		Copy(src_val, val);
-		memcpy((void*)&src_val, (void*)&val, sizeof(ValType));
+		Copy(src_val, &val);
+		memcpy((void*)src_val, (void*)&val, sizeof(ValType));
 	};
+
+	static void FlipArray(ValType* src_val, int nitems)
+	{
+		for (int i=0; i < nitems; i++)
+			Flip(&(src_val[i]));
+	}
 
 	operator ValType () const 
 	{
