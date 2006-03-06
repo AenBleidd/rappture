6a4823dfbbb56f47d0a8eaa2b3184f271602cc39
cxsong <cxsong@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2006-03-06 22:13:52 +0000

    moved to src/mesh dir

---
+++ trunk/src/mesh/serializer.h
@@ -0,0 +1,46 @@
+#ifndef __SERIALIZER_H__
+#define __SERIALIZER_H__
+
+#include <vector>
+#include <map>
+#include "serializable.h"
+
+//
+// class managing serializable objects
+//
+class RpSerializer {
+public:
+
+	RpSerializer() { m_index = 0; };
+
+	// remove all objects, reset ref counts to zero.
+	clear();
+
+	void addObject(RpSerializable* obj);
+
+	void deleteObject(RpSerializable* obj);
+	void deleteObject(const char* objectName);
+
+	// marshalling
+	char* serialize();
+
+	// unmarshalling - instantiate all objects
+	void deserialize(const char* buf);
+
+	// retrieve object
+	Serializable* getObject(const char* objectName);
+
+private:
+	typedef map<const char*, Serializable*> typeObjMap;
+	typedef map<const char*, int> typeRefCount;
+
+	// archive of objects referenced by their names (e.g., output.field)
+	typeObjMap m_objMap;
+
+	// reference count for each object
+	typeRefCount m_refCount;
+
+	unsigned int m_numObjects; //? can get from map.size()
+};
+
+#endif
