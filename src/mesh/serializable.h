5108d4ae7afe7352df58e9a4b4366b59e361f28c
cxsong <cxsong@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2006-03-07 21:50:28 +0000

    modified interface

--- src/mesh/serializable.h
+++ src/mesh/serializable.h
@@ -1,6 +1,10 @@
 #ifndef __SERIALIZABLE_H__
 #define __SERIALIZABLE_H__
 
+#include "util.h"
+
+#define HEADER_SIZE 16
+
 //
 // base class for serializable rappture objects
 //
@@ -9,19 +13,25 @@ public:
 	// return object name (e.g., output.field(f1d))
 	virtual const char* objectName() = 0;
 
+	// set object name
+	virtual void objectName(const char* name) = 0;
+
 	// return object type (e.g., FIELD, MESH3D)
 	virtual const char* objectType() = 0;
 
 	// object marshalling
-	virtual void serialize() = 0;
+	virtual char* serialize(int& nbytes) = 0;
 
 	// object unmarshalling
-	virtual void deserialize() = 0;
+	virtual RP_ERROR deserialize(const char* buf) = 0;
 
 	virtual int size() = 0;
 	virtual int numBytes() = 0;
 
 	virtual ~RpSerializable() { };
+
+	// factory methods
+	//virtual RpSerializable* create(const char* objectType);
 };
 
 #endif
