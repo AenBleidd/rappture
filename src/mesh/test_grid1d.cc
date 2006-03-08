0ce89716622703cae1e4d914abfb3336b7d238c0
cxsong <cxsong@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2006-03-08 19:26:58 +0000

    changed to use the new RpGrid1d

--- src/mesh/test_grid1d.cc
+++ src/mesh/test_grid1d.cc
@@ -26,29 +26,6 @@ static double points[20] = {
 	0.368703495654
 };
 
-static double points2[20][2] = {
-	0.309504615287, 0.844069129715,
-	0.269863116215, 0.589394224151,
-	0.948725301748, 0.226146486693,
-	0.844001851438, 0.139117111982,
-	0.141301087635, 0.847379885543,
-	0.913736318664, 0.166307778641,
-	0.134835615817, 0.182195034894,
-	0.151951467689, 0.848317450773,
-	0.671395143807, 0.138181958412,
-	0.424175028887, 0.10971049923,
-	0.904360553671, 0.587825545849,
-	0.58394908746, 0.432312932998,
-	0.88346489886, 0.394555137211,
-	0.288191101648, 0.627845397977,
-	0.197603800892, 0.127081587039,
-	0.86023335618, 0.942017313532,
-	0.484988525736, 0.202152039484,
-	0.56932761081, 0.689154883236,
-	0.62612255133, 0.241720210408,
-	0.591576322723, 0.623256005171
-};
-
 int main()
 {
 	int i, j, err;
@@ -56,15 +33,15 @@ int main()
 	printf("Testing RpGrid1d\n");
 
 	printf("Testing constructor\n");
-	RpGrid1d grid1(20);
+	RpGrid1d grid1("output.grid(g1d)", 20);
 
-	printf("vector size: %d\n", grid1.numPoints());
+	printf("vector size: %d\n", grid1.size());
 	printf("capacity: %d\n", grid1.capacity());
 
 	printf("Testing addAllPoints\n");
 	grid1.addAllPoints(&(points[0]), 20);
 
-	printf("vector size: %d\n", grid1.numPoints());
+	printf("vector size: %d\n", grid1.size());
 
 	printf("Testing print\n");
 	grid1.print();
@@ -79,42 +56,22 @@ int main()
 	fwrite(buf, 1, nbytes, fp);
 	fclose(fp);
 
-	printf("Testing deserialize\n");
-	grid1.deserialize(buf);
+	printf("Testing print\n");
 	grid1.print();
 
-	printf("Testing instantiation from byte stream\n");
-	RpGrid1d * grid2;
+	printf("Testing deserialize\n");
 
-	grid2 = new RpGrid1d(buf);
+	RpGrid1d * grid2 = new RpGrid1d("test.path");
+	grid2->deserialize(buf);
 
-	grid2->print();
-
-	printf("Testing grid2.data()\n");
-
-	double* ptr = grid2->data();
-	for (i=0; i<grid2->numPoints(); i++) {
-		printf("%.12f \n", ptr[i]);
+	printf("Testing getData\n");
+	
+	double* ptr = grid2->getDataCopy();
+	for (i=0; i<grid2->size(); i++) {
+		printf("%.12f\n", ptr[i]);
 	}
 
-	/*
-	printf("Testing addPoint\n");
 
-	RpGrid1d grid3(20);
-	grid3.addPoint(points[0]);
-	grid3.addPoint(points[1]);
-	grid3.addPoint(points[2]);
-
-	printf("size=%d\n", grid3.numPoints());
-
-	grid3.print();
-
-	printf("Testing data()\n");
-	printf("size=%d\n", grid3.numPoints());
-	double* ptr = grid3.data();
-	for (i=0; i<grid3.numPoints(); i++) {
-		printf("%.12f \n", ptr[i]);
-	}*/
 
 	return 0;
 }
