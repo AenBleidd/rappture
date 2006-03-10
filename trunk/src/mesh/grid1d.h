ee2b06e2f3d488c1ba72ba98031c50fada7a2153
cxsong <cxsong@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2006-03-10 17:59:25 +0000

    made methods virtual for field derivation

--- trunk/src/mesh/grid1d.h
+++ trunk/src/mesh/grid1d.h
@@ -18,55 +18,56 @@ public:
 	// constructors
 	RpGrid1d();
 	RpGrid1d(int size);
-	RpGrid1d(const char* objectName, int size=0);
+	RpGrid1d(const char* objectName, int size);
 	RpGrid1d(DataValType* data, int size); // makes a copy of data
 
-	void objectName(const char* str);
-	const char* objectName();
-	const char* objectType();
+	// constructor for a regular grid with start point and delta
+	RpGrid1d(DataValType startPoint, DataValType delta, int npts);
+
+	virtual void objectName(const char* str);
+	virtual const char* objectName();
+	virtual const char* objectType();
 
 	// return number of bytes needed for serialization
-	int numBytes(); 
+	virtual int numBytes(); 
 
 	// return number of points in grid
-	int size() { return numPoints(); };
-
+	virtual int size() { return numPoints(); };
 
 	// add all points to grid
-	RP_ERROR addAllPoints(DataValType* val, int nitems);
+	virtual RP_ERROR addAllPoints(DataValType* val, int nitems);
 
 	// add one point to grid
-	void addPoint(DataValType val);
+	virtual void addPoint(DataValType val);
 
 	// return number of points in grid
 
 	// max num points that can be stored
-	int capacity() { return m_data.capacity(); }
-
+	virtual int capacity() { return m_data.capacity(); }
 
 	// change the size of the grid after grid is constructed
-	void resize(int npoints) { m_data.resize(npoints); }
+	virtual void resize(int npoints) { m_data.resize(npoints); }
 
 	// return pointer to array of points
-	DataValType* getData();
+	virtual DataValType* getData();
 
 	// return copy of data array - caller must free memory when
 	// not done
-	DataValType* getDataCopy();
+	virtual DataValType* getDataCopy();
 
 	// serialize data 
 	// returns pointer to buffer 
 	// number of bytes (nbytes) set
 	//
-	char * serialize(int& nbytes);
-	RP_ERROR doSerialize(char * buf, int nbytes);
+	virtual char * serialize(int& nbytes);
+	virtual RP_ERROR doSerialize(char * buf, int nbytes);
 
 	// deserialize data
-	RP_ERROR deserialize(const char* buf);
-	RP_ERROR doDeserialize(const char* buf);
+	virtual RP_ERROR deserialize(const char* buf);
+	virtual RP_ERROR doDeserialize(const char* buf);
 
-	void xmlString(std::string& str);
-	void print();
+	virtual void xmlString(std::string& str);
+	virtual void print();
 
 	// destructor
 	virtual ~RpGrid1d() { };
@@ -74,9 +75,9 @@ public:
 protected:
 	int numPoints() { return m_data.size(); };
 
-private:
 	std::string m_name; // object name
 	vector<DataValType> m_data; // array of doubles
+
 };
 
 #endif
