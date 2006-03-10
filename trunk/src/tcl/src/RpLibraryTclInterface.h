e8e719875e662551c560d2b05a1ad3260fa712c2
dkearney <dkearney@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2006-03-10 22:19:27 +0000

    forgot to add this file previously, for tcl bindings.

---
+++ trunk/src/tcl/src/RpLibraryTclInterface.h
@@ -0,0 +1,14 @@
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int Rappturelibrary_Init _ANSI_ARGS_((Tcl_Interp * interp));
+
+
+#define RAPPTURE_OBJ_TYPE "::Rappture::LibraryObj"
+
+#ifdef __cplusplus
+}
+#endif
+
