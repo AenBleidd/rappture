ae832f15a51fcb965cb1b50e98707a4176de2194
cxsong <cxsong@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2006-03-14 04:03:54 +0000

    replaced cons strings with RpCurrentVersion[]

--- trunk/src/mesh/const.cc
+++ trunk/src/mesh/const.cc
@@ -30,8 +30,24 @@ const char* RpObjectTypes[] = {
 	"CURVE2D"
 };
 
+const char* RpCurrentVersion[] = {
+	"RV-A-BOOLEAN",
+	"RV-A-NUMBER",
+	"RV-A-STRING",
+	"RV-A-FIELD",
+	"RV-A-MESH3D",
+	"RV-A-ELEMENT",
+	"RV-A-NODE2D",
+	"RV-A-NODE3D",
+	"RV-A-GRID1D",
+	"RV-A-GRID2D",
+	"RV-A-GRID3D",
+	"RV-A-CURVE2D"
+};
+
 //const int HEADER_SIZE = 16;
 
+/*
 const char* RpGrid1d_current_version = "RV-A-GRID1D";
 const char* RpGrid2d_current_version = "RV-A-GRID2D";
 const char* RpMesh3d_current_version = "RV-A-MESH3D";
@@ -40,4 +56,5 @@ const char* RpCurve_current_version = "RV-A-CURVE";
 const char* RpElement_current_version = "RV-A-ELEMENT";
 const char* RpNode2d_current_version = "RV-A-NODE2D";
 const char* RpNode3d_current_version = "RV-A-NODE3D";
+*/
 
