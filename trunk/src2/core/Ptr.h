55da69950a734727e9cda33ad70be34755e65ef8
mmc <mmc@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2006-05-21 19:56:38 +0000

    Cleaned up smart pointers and added Dictionary class.

---
+++ trunk/src2/core/Ptr.h
@@ -0,0 +1,168 @@
+/*
+ * ======================================================================
+ *  Rappture::Ptr<type>
+ *
+ *  AUTHOR:  Michael McLennan, Purdue University
+ *  Copyright (c) 2004-2006  Purdue Research Foundation
+ * ----------------------------------------------------------------------
+ *  See the file "license.terms" for information on usage and
+ *  redistribution of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+ * ======================================================================
+ */
+#ifndef RAPPTURE_PTR_H
+#define RAPPTURE_PTR_H
+
+#include "rappture.h"
+
+namespace Rappture {
+
+/**
+ *  This is the core of a smart pointer, built to keep a reference
+ *  count and do most of the work, so the template class can be lean
+ *  and mean.
+ */
+class PtrCore {
+public:
+    explicit PtrCore(void* ptr=NULL);
+    ~PtrCore();
+
+    void* pointer() const;
+    void attach();
+    void* detach();
+
+private:
+    // copying the core is not allowed
+    PtrCore(const PtrCore& pc) { assert(0); }
+    PtrCore& operator=(const PtrCore& pc) { assert(0); }
+
+    void *_ptr;
+    int _refcount;
+};
+
+/**
+ *  This is a smart pointer with reference counting.  Once one of
+ *  these pointers is constructed with an object of the underlying
+ *  type, that object becomes property of the pointer.  Other
+ *  pointers can point to the same object.  When all such pointers
+ *  have been destroyed, the underlying object goes away.
+ */
+template <class Type>
+class Ptr {
+public:
+    explicit Ptr(Type* ptr=NULL);
+    Ptr(const Ptr& ptr);
+    Ptr& operator=(Type* ptr);
+    Ptr& operator=(const Ptr& ptr);
+    ~Ptr();
+
+    int isNull() const;
+    Type* operator->() const;
+    Type* pointer() const;
+    Type& operator*() const;
+    void clear();
+
+private:
+    PtrCore *_pc;
+};
+
+template <class Type>
+Ptr<Type>::Ptr(Type* ptr)
+  : _pc(NULL)
+{
+    if (ptr) {
+        _pc = new PtrCore(ptr);
+    }
+}
+
+template <class Type>
+Ptr<Type>::Ptr(const Ptr<Type>& ptr)
+  : _pc(NULL)
+{
+    if (ptr._pc) {
+        ptr._pc->attach();
+        _pc = ptr._pc;
+    }
+}
+
+template <class Type>
+Ptr<Type>::~Ptr()
+{
+    clear();
+}
+
+template <class Type>
+Ptr<Type>&
+Ptr<Type>::operator=(Type* ptr)
+{
+    clear();
+    _pc = new PtrCore(ptr);
+}
+
+template <class Type>
+Ptr<Type>&
+Ptr<Type>::operator=(const Ptr<Type>& ptr)
+{
+    if (ptr._pc) {
+        ptr._pc->attach();
+    }
+    clear();
+    _pc = ptr._pc;
+}
+
+template <class Type>
+int
+Ptr<Type>::isNull() const
+{
+    if (_pc) {
+        return (_pc->pointer() == NULL);
+    }
+    return 1;
+}
+
+template <class Type>
+Type*
+Ptr<Type>::operator->() const
+{
+    if (_pc) {
+        return static_cast<Type*>(_pc->pointer());
+    }
+    return NULL;
+}
+
+template <class Type>
+Type*
+Ptr<Type>::pointer() const
+{
+    if (_pc) {
+        return _pc->pointer();
+    }
+    return NULL;
+}
+
+template <class Type>
+Type&
+Ptr<Type>::operator*() const
+{
+    assert(_pc != NULL);
+    return *(Type*)_pc->pointer();
+}
+
+template <class Type>
+void
+Ptr<Type>::clear()
+{
+    if (_pc) {
+        Type* ptr = (Type*)_pc->detach();
+        if (ptr) {
+            // If we get back a pointer, then it is fully detached.
+            // Clean it up.
+            delete ptr;
+            delete _pc;
+        }
+        _pc = NULL;
+    }
+}
+
+} // namespace Rappture
+
+#endif
--- trunk/src2/core/test.cc
+++ trunk/src2/core/test.cc
@@ -1,18 +1,8 @@
 #include <stdlib.h>
 #include <iostream>
 #include <sstream>
-#include "RpPtr.h"
-#include "RpOutcome.h"
-#include "RpNode.h"
-#include "RpField1D.h"
-#include "RpMeshTri2D.h"
-#include "RpFieldTri2D.h"
-#include "RpMeshRect3D.h"
-#include "RpFieldRect3D.h"
-#include "RpMeshPrism3D.h"
-#include "RpFieldPrism3D.h"
-#include "RpSerialBuffer.h"
-#include "RpSerializer.h"
+#include "rappture.h"
+#include "Dictionary.h"
 
 class Foo {
   char *desc;
@@ -36,14 +26,6 @@ foo(int code) {
     return status;
 }
 
-void
-showmesh(Rappture::Mesh1D& mesh) {
-    for (int i=0; i < mesh.size(); i++) {
-        std::cout << mesh.at(i).id() << "=" << mesh.at(i).x() << " ";
-    }
-    std::cout << std::endl;
-}
-
 int
 main(int argc, char* argv[]) {
     Rappture::Ptr<Foo> ptr(new Foo("a"));
@@ -57,112 +39,19 @@ main(int argc, char* argv[]) {
     std::cout << "clear ptr(barney)" << std::endl;
     ptr.clear();
 
-    Rappture::Field1D f;
-    f.define(1.1, 1);
-    f.define(3.3, 2);
-    f.define(7.7, 3);
+    Rappture::Dictionary<int,int> str2id;
+    str2id.get(4,NULL).value() = 1;
+    str2id.get(105,NULL).value() = 2;
+    str2id.get(69,NULL).value() = 3;
+    str2id.get(95,NULL).value() = 4;
 
-    std::cout << "@   0 = " << f.value(0) << std::endl;
-    std::cout << "@ 2.2 = " << f.value(2.2) << std::endl;
-    std::cout << "@ 5.5 = " << f.value(5.5) << std::endl;
-    std::cout << "@ 5.6 = " << f.value(5.6) << std::endl;
-    std::cout << "@ 7.6 = " << f.value(7.6) << std::endl;
-    std::cout << "@ 7.7 = " << f.value(7.7) << std::endl;
-    std::cout << "@ 8.8 = " << f.value(8.8) << std::endl;
+    std::cout << str2id.stats();
 
-    Rappture::Ptr<Rappture::Mesh1D> uni(
-        new Rappture::Mesh1D(1.0,5.0,16)
-    );
-    showmesh(*uni);
-
-    Rappture::Mesh1D uniy(0.0,2.0,10);
-    Rappture::Mesh1D uniz(-1.0,1.0,20);
-    Rappture::Ptr<Rappture::MeshRect3D> m3dPtr(
-        new Rappture::MeshRect3D(*uni, uniy, uniz)
-    );
-    Rappture::Ptr<Rappture::FieldRect3D> f3d(
-        new Rappture::FieldRect3D(*uni, uniy, uniz)
-    );
-
-    int n=0;
-    for (int iz=0; iz < f3d->size(Rappture::zaxis); iz++) {
-        double zval = f3d->atNode(Rappture::zaxis, iz).x();
-        for (int iy=0; iy < f3d->size(Rappture::yaxis); iy++) {
-            double yval = f3d->atNode(Rappture::yaxis, iy).x();
-            for (int ix=0; ix < f3d->size(Rappture::xaxis); ix++) {
-                double xval = f3d->atNode(Rappture::xaxis, ix).x();
-                f3d->define(n++, xval*yval*zval);
-            }
-        }
+    Rappture::DictEntry<int,int> entry = str2id.first();
+    while (!entry.isNull()) {
+        std::cout << " " << entry.key() << " = =" << entry.value() << std::endl;
+        entry.next();
     }
-
-    std::cout << "at (1,0,-0.5) " << f3d->value(1.0,0.0,-0.5) << std::endl;
-    std::cout << "at (2,1,-0.5) " << f3d->value(2.0,1.0,-0.5) << std::endl;
-    std::cout << "at (3,0.7,-0.2) " << f3d->value(3.0,0.7,-0.2) << std::endl;
-    std::cout << "at (5,2,1) " << f3d->value(5.0,2.0,1.0) << std::endl;
-
-
-    Rappture::MeshTri2D m2d;
-    m2d.addNode( Rappture::Node2D(0.0,0.0) );
-    m2d.addNode( Rappture::Node2D(1.0,0.0) );
-    m2d.addNode( Rappture::Node2D(0.0,1.0) );
-    m2d.addNode( Rappture::Node2D(1.0,1.0) );
-    m2d.addNode( Rappture::Node2D(2.0,0.0) );
-    m2d.addNode( Rappture::Node2D(2.0,1.0) );
-    m2d.addCell(0,1,2);
-    m2d.addCell(1,2,3);
-    m2d.addCell(1,3,4);
-    m2d.addCell(3,4,5);
-
-    Rappture::FieldTri2D f2d(m2d);
-    f2d.define(0, 0.0);
-    f2d.define(1, 1.0);
-    f2d.define(2, 1.0);
-    f2d.define(3, 4.0);
-    f2d.define(4, 2.0);
-    f2d.define(5, 2.0);
-    std::cout << "value @(0.1,0.1): " << f2d.value(0.1,0.1) << std::endl;
-    std::cout << "value @(0.99,0.99): " << f2d.value(0.99,0.99) << std::endl;
-    std::cout << "value @(1.99,0.01): " << f2d.value(1.99,0.01) << std::endl;
-    std::cout << "value @(2.01,0.02): " << f2d.value(2.01,1.02) << std::endl;
-
-    Rappture::FieldPrism3D fp3d(m2d, uniy);
-    for (int i=0; i < 10; i++) {
-        fp3d.define(i*6+0, (i+1)*0.0);
-        fp3d.define(i*6+1, (i+1)*1.0);
-        fp3d.define(i*6+2, (i+1)*1.0);
-        fp3d.define(i*6+3, (i+1)*4.0);
-        fp3d.define(i*6+4, (i+1)*2.0);
-        fp3d.define(i*6+5, (i+1)*2.0);
-    }
-    std::cout << "value @(0.1,0.1,0.01): " << fp3d.value(0.1,0.1,0.01) << std::endl;
-    std::cout << "value @(0.1,0.1,0.21): " << fp3d.value(0.1,0.1,0.21) << std::endl;
-    std::cout << "value @(0.1,0.1,0.41): " << fp3d.value(0.1,0.1,0.41) << std::endl;
-    std::cout << "value @(0.99,0.99,0.01): " << fp3d.value(0.99,0.99,0.01) << std::endl;
-    std::cout << "value @(0.99,0.99,0.21): " << fp3d.value(0.99,0.99,0.21) << std::endl;
-    std::cout << "value @(0.99,0.99,0.41): " << fp3d.value(0.99,0.99,0.41) << std::endl;
-
-
-    std::cout << "original mesh:" << std::endl;
-    showmesh( *uni );
-    Rappture::Serializer objs;
-    objs.add( uni.pointer() );
-    Rappture::Ptr<Rappture::SerialBuffer> buffer = objs.serialize();
-
-    Rappture::Serializer objs2;
-    Rappture::Outcome err = objs2.deserialize(buffer->bytes(), buffer->size());
-
-    if (err) {
-        err.addContext("while in main program");
-        std::cout << err.remark() << std::endl;
-        std::cout << err.context() << std::endl;
-        exit(1);
-    }
-
-    Rappture::Ptr<Rappture::Serializable> uni2ptr = objs2.get(0);
-    Rappture::Ptr<Rappture::Mesh1D> uni2( (Rappture::Mesh1D*)uni2ptr.pointer() );
-    std::cout << "reconstituted mesh:" << std::endl;
-    showmesh( *uni2 );
 
     exit(0);
 }
