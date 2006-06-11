429c6c5fbb2a5f3a8714111302862b39d36c3354
mmc <mmc@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2006-06-11 22:13:27 +0000

    Oops!  Two more problems related to downloads.  The DeviceResult and DeviceEditor didn't have proper args to support "download controls". CNTbands was misbehaving when you downloaded the molecular structure.

--- gui/scripts/deviceresult.tcl
+++ gui/scripts/deviceresult.tcl
@@ -27,7 +27,7 @@ itcl::class Rappture::DeviceResult {
     public method get {}
     public method delete {args}
     public method scale {args}
-    public method download {option}
+    public method download {option args}
 
     set _dataobj ""  ;# data object currently being displayed
 }
@@ -117,6 +117,7 @@ itcl::body Rappture::DeviceResult::scale {args} {
 
 # ----------------------------------------------------------------------
 # USAGE: download coming
+# USAGE: download controls <downloadCommand>
 # USAGE: download now
 #
 # Clients use this method to create a downloadable representation
@@ -124,6 +125,6 @@ itcl::body Rappture::DeviceResult::scale {args} {
 # "ext" is the file extension (indicating the type of data) and
 # "string" is the data itself.
 # ----------------------------------------------------------------------
-itcl::body Rappture::DeviceResult::download {option} {
-    return [$itk_component(viewer) download $option]
+itcl::body Rappture::DeviceResult::download {option args} {
+    return [eval $itk_component(viewer) download $option $args]
 }
