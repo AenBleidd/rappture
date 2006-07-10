058099b27a813b4a1e92d9239e95f3463f8fc584
nkissebe <nkissebe@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2006-07-10 18:33:08 +0000

    app-fermi example for perl bindings

---
+++ examples/app-fermi/perl/fermi.pl
@@ -0,0 +1,39 @@
+# ----------------------------------------------------------------------
+#  EXAMPLE: Fermi-Dirac function in Perl.
+#         
+#  This simple example shows how to use Rappture within a simulator
+#  written in Perl.
+# ======================================================================
+#  AUTHOR:  Nicholas J. Kisseberth, Purdue University
+#  Copyright (c) 2006  Purdue Research Foundation
+#       
+#  See the file "license.terms" for information on usage and
+#  redistribution of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+# ======================================================================
+
+use Rappture;
+
+# open the XML file containing the run parameters
+
+$driver = Rappture::RpLibrary->new($ARGV[0]);
+
+$Tstr = $driver->get("input.(temperature).current");
+$T = Rappture::RpUnits::convert($Tstr, "K", "off");
+
+$Efstr = $driver->get("input.(Ef).current");
+$Ef = Rappture::RpUnits::convert($Efstr, "eV", "off");
+
+$kT = 8.61734e-5 * $T;
+$Emin = $Ef - 10 * $kT;
+$Emax = $Ef + 10 * $kT;
+
+$E = $Emin;
+$dE = 0.005*($Emax - $Emin);
+
+while( $E < $Emax ) {
+	$f = 1.0 / ( 1.0 + exp(($E - $Ef) / $kT));
+	$driver->put("output.curve(f12).component.xy", "$f $E\n", 1);
+	$E = $E + $dE;
+}
+
+$driver->result();
