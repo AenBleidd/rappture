60793430ad4a129199d730373eab5868c79cd66e
nkissebe <nkissebe@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2006-07-11 16:10:02 +0000

    null_terminate_str can't have extern "C" interface (Microsoft C++ enforces)

--- include/fortran/RpFortranCommon.h
+++ include/fortran/RpFortranCommon.h
@@ -21,13 +21,13 @@
 #ifndef _RpFORTRAN_COMMON_H
 #define _RpFORTRAN_COMMON_H
 
+std::string null_terminate_str(const char* inStr, int len);
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 char* null_terminate(char* inStr, int len);
-std::string null_terminate_str(const char* inStr, int len);
 void fortranify(const char* inBuff, char* retText, int retTextLen);
 
 #ifdef __cplusplus
