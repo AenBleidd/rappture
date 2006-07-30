2cb9a92f72a5128e04cf8b8a72e24ca4f1963bf3
dkearney <dkearney@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2006-07-30 06:07:47 +0000

    added checks to configure.in to find where python and perl live, added .in files for python and perl build files configure now builds perl/Makefile.PL and python/setup.py and fits it with the correct values needed to build and install the module in the correct directory, where it will live in the rappture distribution. there had been talk of using env variables to point python and perl to where these modules live, so that is the current line of thinking. added queue module for python. the Rappture.queue module's purpose is to make it easy to send a job to the pbs and condor queues and track the job using rappture. pbs is kinda complete, condor is not complete and is still being worked on. the signalHandler and tools python modules came from left over code that all python rappture applications might want to include to make the developers life a little more enjoyable.

---
+++ python/Rappture/signalHandler.py
@@ -0,0 +1,78 @@
+# ----------------------------------------------------------------------
+#
+# ======================================================================
+#  AUTHOR:  Derrick Kearney, Purdue University
+#  Copyright (c) 2005  Purdue Research Foundation, West Lafayette, IN
+# ======================================================================
+
+import sys
+import signal
+
+qID = []
+queue_type = ''
+
+def sigGEN_handler(signal, frame):
+    global qID
+    global queue_type
+    if qID :
+        for id in xrange(len(qID)):
+            sys.stdout.write("Killing job with qID = :%s:\n" % (qID[id]))
+            sys.stdout.flush()
+            if queue_type == 'PBS':
+                rmCmdOut = getCommandOutput('qdel %s' % (qID[id]))
+            elif queue_type == 'Condor':
+                rmCmdOut = getCommandOutput('condor_rm %s' % (qID[id]))
+            else:
+                # unrecognized queue_type
+                break
+            sys.stdout.write("\n%s\n" % rmCmdOut)
+            sys.stdout.flush()
+            time.sleep(10)
+    sys.stdout.write("\nEXITING...\n")
+    sys.stdout.flush()
+    sys.exit(-1)
+
+
+def sigINT_handler(signal, frame):
+    sys.stdout.write('Received SIGINT!\n')
+    sys.stdout.flush()
+    sigGEN_handler(signal, frame)
+
+def sigHUP_handler(signal, frame):
+    sys.stdout.write('Received SIGHUP!\n')
+    sys.stdout.flush()
+    sigGEN_handler(signal, frame)
+
+def sigQUIT_handler(signal, frame):
+    sys.stdout.write('Received SIGQUIT!\n')
+    sys.stdout.flush()
+    sigGEN_handler(signal, frame)
+
+def sigABRT_handler(signal, frame):
+    sys.stdout.write('Received SIGABRT!\n')
+    sys.stdout.flush()
+    sigGEN_handler(signal, frame)
+
+def sigTERM_handler(signal, frame):
+    sys.stdout.write('Received SIGTERM!\n')
+    sys.stdout.flush()
+    sigGEN_handler(signal, frame)
+
+def sigSTOP_handler(signal, frame):
+    sys.stdout.write('Received SIGSTOP!\n')
+    sys.stdout.flush()
+    sigGEN_handler(signal, frame)
+
+def sigKILL_handler(signal, frame):
+    sys.stdout.write('Received SIGKILL!\n')
+    sys.stdout.flush()
+    sigGEN_handler(signal, frame)
+
+signal.signal(signal.SIGINT, sigINT_handler)
+signal.signal(signal.SIGHUP, sigHUP_handler)
+signal.signal(signal.SIGQUIT, sigQUIT_handler)
+signal.signal(signal.SIGABRT, sigABRT_handler)
+signal.signal(signal.SIGTERM, sigTERM_handler)
+#signal.signal(signal.SIGSTOP, sigSTOP_handler)
+#signal.signal(signal.SIGKILL, sigKILL_handler)
+
