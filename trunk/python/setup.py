68911d718f78b6660aff97882482a159d70d1367
dkearney <dkearney@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2006-09-20 17:20:11 +0000

    removing makefiles and setup files that have been replaced by files automatically created by configure

--- trunk/python/setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-setup(name='Rappture',
-  version='0.1',
-  description='Rapid Application Infrastructure library for nanoHUB.org',
-  url='http://www.nanohub.org/',
-  py_modules=['Rappture.library','Rappture.interface','Rappture.number','Rappture.result'],
-  )
