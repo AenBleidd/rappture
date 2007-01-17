84b497239cbbd70b00768dcfc2da8bd2e0363be7
kennell <kennell@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2007-01-17 22:29:31 +0000

    Added a "connect" parameter to forward connection through port router. Added a snooze() method to sleep. Changed reconnect logic to handle reconnects after host app exits. Modified file load to use https if connect parameter is specified.

--- gui/filexfer/monitor.java
+++ gui/filexfer/monitor.java
@@ -26,6 +26,7 @@ public class monitor extends Thread {
     private String user;
     private String ipAddr;
     private String cookie;
+    private String connect_param;
 
     private Socket socket = null;
     public BufferedReader istream;
@@ -34,13 +35,26 @@ public class monitor extends Thread {
     private boolean run = true;
 
     public monitor(filexfer parent, String hostName, int hostPort,
-          String user, String ipAddr, String cookie) {
+          String user, String ipAddr, String cookie, String connect_param) {
         this.parent = parent;
         this.hostName = hostName;
         this.hostPort = hostPort;
         this.user = user;
         this.ipAddr = ipAddr;
         this.cookie = cookie;
+        this.connect_param = connect_param;
+    }
+
+    private void snooze(int sec) {
+        int ms = sec * 1000;
+        parent.status.append("Snoozing " + Integer.toString(sec) + " sec.\n");
+        try {
+            // sleep a little, so we don't chew up CPU
+            sleep(ms);
+        }
+        catch (java.lang.InterruptedException e) { 
+            // just move on
+        }
     }
 
     public void run() {
@@ -48,31 +62,42 @@ public class monitor extends Thread {
         while (run) {
             if (socket == null) {
                 if (!connect()) {
-                    try {
-                        // sleep a little, so we don't chew up CPU
-                        sleep(5000);
-                    }
-                    catch (java.lang.InterruptedException e) { 
-                        // just move on
-                    }
+                    snooze(60);
                 }
             } else {
                 try {
-                    line = istream.readLine();
-                    if (line == null) {
-                        parent.status.append("CLOSED\n");
-                        socket.close();
-                        socket = null;
-                    } else {
-                        handle(line);
+                    while(true) {
+                        line = istream.readLine();
+                        if (line == null) {
+                            parent.status.append("CLOSED\n");
+                            socket.close();
+                            socket = null;
+                        } else {
+                            handle(line);
+                        }
                     }
                 }
                 catch (java.io.IOException e){
                     parent.status.append("I/O error: "+e.getMessage()+"\n");
+                    try {
+                        socket.close();
+                    }
+                    catch (Exception e2) {
+                        // Ignore errors.
+                    }
+                    socket = null;
                 }
                 catch (Exception e) {
                     parent.status.append("Internal error: "+e.getMessage()+"\n");
+                    try {
+                        socket.close();
+                    }
+                    catch (Exception e2) {
+                        // Ignore errors.
+                    }
+                    socket = null;
                 }
+                snooze(60);
             }
         }
     }
@@ -136,6 +161,36 @@ public class monitor extends Thread {
             return false;
         }
 
+        if (connect_param != null) {
+            try {
+                ostream.println("CONNECT " + connect_param + " HTTP/1.1");
+                ostream.println("Host: " + connect_param);
+                ostream.println("");
+            }
+            catch (Exception e) {
+                parent.status.append("\nCan't forward connection.");
+                return false;
+            }
+            try {
+                while(true) {
+                    String line = istream.readLine();
+                    if (line.length() == 0) {
+                        parent.status.append("\nDone with forward.");
+                        break;
+                    }
+                    if (line == null) {
+                        parent.status.append("\nNull forward.");
+                        break;
+                    }
+                    parent.status.append("\n" + Integer.toString(line.length()) + ":" + line);
+                }
+            }
+            catch (Exception e) {
+                parent.status.append("\nCan't get forward response.");
+                return false;
+            }
+        }
+
         try {
             ostream.println("REGISTER "+user+" "+ipAddr+" "+cookie+" RAPPTURE/"+protocol);
             ostream.println("ACTIVATE RAPPTURE");
@@ -143,6 +198,7 @@ public class monitor extends Thread {
         }
         catch (Exception e) {
             parent.status.append("\nCan't talk to server");
+            return false;
         }
         return true;
     }
@@ -152,8 +208,15 @@ public class monitor extends Thread {
 
         if (line.startsWith("url ")) {
             try {
-                URL url = new URL("http",
-                    hostName, hostPort, line.substring(4));
+                URL url = null;
+                if (connect_param == null) {
+                    url = new URL("http",
+                                  hostName, hostPort, line.substring(4));
+                } else {
+                    url = new URL("https", hostName,
+                                  "/" + connect_param.replace(':','/') +
+                                  line.substring(4));
+                }
 
                 try {
                     parent.getAppletContext().showDocument(url,"_blank");
