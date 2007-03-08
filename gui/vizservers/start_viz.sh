9b30556883909d13cc8b7e0a201006a47ba9d57c
dkearney <dkearney@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2007-03-08 21:33:01 +0000

    adjusted start_vis.sh to take into account new scheme for starting vis server where the full path of the nanovis program is used to determine the the path for resources and shaders directories

--- gui/vizservers/start_viz.sh
+++ gui/vizservers/start_viz.sh
@@ -1,9 +1,14 @@
 #! /bin/sh
 
+DISPLAY=:0
+export DISPLAY
+
 dir=$( mktemp -d /tmp/nanorun.XXXXXX )
 
+cp -r nanovis/shaders ${dir}
+cp -r nanovis/resources ${dir}
 cp nanoscale/nanoscale nanovis/nanovis ${dir}
 
 cd ${dir}
 
-./nanoscale -l 2000 -b 2000 -c 172.18.3.255 -c ./nanovis
+./nanoscale -l 2000 -b 2000 -c 172.18.3.255 -c ${dir}/nanovis
