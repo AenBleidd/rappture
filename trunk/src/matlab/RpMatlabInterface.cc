a194a96cb2c23beef037228103c27bc920d98b1b
dkearney <dkearney@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2007-05-16 20:27:15 +0000

    added the rpmxFlush function to flush matlab's buffers on demand. this is used in the progress function.

--- trunk/src/matlab/RpMatlabInterface.cc
+++ trunk/src/matlab/RpMatlabInterface.cc
@@ -4,7 +4,7 @@
  *
  * ======================================================================
  *  AUTHOR:  Derrick Kearney, Purdue University
- *  Copyright (c) 2004-2005  Purdue Research Foundation
+ *  Copyright (c) 2004-2007  Purdue Research Foundation
  *
  *  See the file "license.terms" for information on usage and
  *  redistribution of this file, and for a DISCLAIMER OF ALL WARRANTIES.
@@ -71,3 +71,18 @@ getStringInput ( const mxArray* prhs ) {
 
     return path;
 }
+
+void
+freeStringInput (void* ptr) {
+    if (ptr != NULL) {
+        mxFree(ptr);
+    }
+    ptr = NULL;
+}
+
+void
+rpmxFlush() {
+    mxArray * emptyStr = mxCreateString("");
+    mexCallMATLAB(0,0,1,&emptyStr,"fprintf");
+}
+
