cc5f5b97a92e0bebe8f3a96e817ba97c1b4f5814
mmc <mmc@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2007-08-01 01:55:06 +0000

    Added original version of fermi.c program (before Rappture conversion)

---
+++ examples/app-fermi/cee/original.c
@@ -0,0 +1,35 @@
+/* ----------------------------------------------------------------------
+ *  EXAMPLE: Fermi-Dirac function in C.
+ * ======================================================================
+ *  AUTHOR:  Michael McLennan, Purdue University
+ *  Copyright (c) 2004-2007  Purdue Research Foundation
+ *
+ *  See the file "license.terms" for information on usage and
+ *  redistribution of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+ * ======================================================================
+ */
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char *argv[]) {
+    double T, Ef, E, dE, kT, Emin, Emax, f;
+
+    printf("Enter the Fermi energy in eV:\n");
+    scanf("%lg", &Ef);
+    printf("Enter the Temperature in K:\n");
+    scanf("%lg", &T);
+
+    kT = 8.61734e-5 * T;
+    Emin = Ef - 10*kT;
+    Emax = Ef + 10*kT;
+
+    E = Emin;
+    dE = 0.005*(Emax-Emin);
+
+    while (E < Emax) {
+        f = 1.0/(1.0 + exp((E - Ef)/kT));
+        printf("%f %f\n",f, E);
+        E = E + dE;
+    }
+    return 0;
+}
