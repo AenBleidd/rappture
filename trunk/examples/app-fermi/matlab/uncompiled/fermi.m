5d18f88fbf8fbc12298013ae8fc68aaa066884e6
dkearney <dkearney@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2008-11-18 19:38:28 +0000

    moving compiled matlab back to its home in the compiled directory. adding uncompiled matlab back to repository, but keeping it in the uncompiled dir. currently only uncompiled matlab is installed. when the compiled version is fixed, we can add it back to being installed. one thing we will need to change in run_app.patch is to use the correct extension, right now we hard code it to glnx86 but on 64bit machines it should be glnxa64. we used to have a MEX_ARCH flag in configure. it should be resurrected and used if matlab cannot tell us the correct extension.

--- trunk/examples/app-fermi/matlab/uncompiled/fermi.m
+++ trunk/examples/app-fermi/matlab/uncompiled/fermi.m
@@ -1,5 +1,5 @@
 % ----------------------------------------------------------------------
-%  EXAMPLE: Fermi-Dirac function in Matlab.
+%  EXAMPLE: Fermi-Dirac function in Octave.
 %
 %  This script represents a newly written application with rappture 
 %  bindings and interface.
@@ -7,16 +7,21 @@
 % ======================================================================
 %  AUTHOR:  Michael McLennan, Purdue University
 %  AUTHOR:  Derrick Kearney, Purdue University
-%  Copyright (c) 2004-2008  Purdue Research Foundation
+%  Copyright (c) 2004-2007  Purdue Research Foundation
 %
 %  See the file "license.terms" for information on usage and
 %  redistribution of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 % ======================================================================
 
-function retVal=fermi;
+% get out input file from the command line
+% invoke this script with the following command:
+% matlab -nodisplay -r infile=\'driver1234.xml\',fermi
+% the above command sets variable infile to the name 'driver1234.xml'
 
-% We open the xml file in the main function and make lib global
-global lib;
+% infile = 'driver31619.xml'
+
+% open our xml input file.
+lib = rpLib(infile);
 
 % retrieve user specified data out of the input file
 % convert values to correct units.
@@ -50,4 +55,4 @@ end
 % signal the end of processing
 rpLibResult(lib);
 
-retVal = 0;
+quit;
