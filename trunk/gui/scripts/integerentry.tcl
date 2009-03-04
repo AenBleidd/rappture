b4189735268ef7b9d8607d92ea3eab18d55598b7
dkearney <dkearney@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2009-03-04 06:21:48 +0000

    various code cleanups, indenting, removing tabs

--- trunk/gui/scripts/integerentry.tcl
+++ trunk/gui/scripts/integerentry.tcl
@@ -71,10 +71,10 @@ itcl::body Rappture::IntegerEntry::constructor {owner path args} {
     # if there is a color, use it for the min/max spectrum
     set color [$_owner xml get $path.about.color]
     if {$color != "" && $min != "" && $max != ""} {
-	# For compatibility. If only one color use white for min
-	if {[llength $color] == 1} {
-	    set color [list $min white $max $color]
-	}
+        # For compatibility. If only one color use white for min
+        if {[llength $color] == 1} {
+            set color [list $min white $max $color]
+        }
         $itk_component(spinner) configure \
             -spectrum [Rappture::Spectrum ::#auto $color]
     }
