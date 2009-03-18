662c25943ff933c47c7a411dea1df87a3ca98cb9
gah <gah@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2009-03-18 22:11:32 +0000

    fixed comment

--- gui/scripts/nanovisviewer.tcl
+++ gui/scripts/nanovisviewer.tcl
@@ -709,9 +709,10 @@ itcl::body Rappture::NanovisViewer::download {option args} {
 	    return ""
 	}
 	now {
-	    # Get image data (as base64) and decode back to binary.  This is
-	    # better than writing to temporary files.  When we switch the BLT
-	    # picture image it won't be necessary to decode the image data.
+	    # Get the image data (as base64) and decode it back to binary.
+	    # This is better than writing to temporary files.  When we switch
+	    # to the BLT picture image it won't be necessary to decode the
+	    # image data.
 	    set bytes [$_image(plot) data -format "jpeg -quality 100"]
 	    set bytes [Rappture::encoding::decode -as b64 $bytes]
 	    return [list .jpg $data]
