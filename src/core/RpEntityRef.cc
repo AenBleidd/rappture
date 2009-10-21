54853577a9cc11aaf9c8cb699311a00070db9d9b
dkearney <dkearney@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2009-10-21 17:57:53 +0000

    adjustments to entity ref encoding, allowing for repeated entity refs like &&

--- src/core/RpEntityRef.cc
+++ src/core/RpEntityRef.cc
@@ -22,8 +22,8 @@
  *        &amp;          "&"
  *        &lt;           "<"
  *        &gt;           ">"
- *	  &quot;	 "\""
- *	  &apos;	 "'"
+ *        &quot;         "\""
+ *        &apos;         "'"
  *
  */
 
@@ -34,10 +34,10 @@
 using namespace Rappture;
 
 typedef struct {
-    const char *replacement;		/* Replacement string. */
-    size_t length;			/* Length of replacement string,
-					 * including the ampersand. */
-    const char *entity;			/* Single character entity. */
+    const char *replacement;                /* Replacement string. */
+    size_t length;                        /* Length of replacement string,
+                                         * including the ampersand. */
+    const char *entity;                        /* Single character entity. */
 } PredefEntityRef;
 
 static PredefEntityRef predef[] = {
@@ -52,9 +52,9 @@ static int nPredefs = sizeof(predef) / sizeof (PredefEntityRef);
 /*
  * EntityRef::decode --
  *
- *	Convert XML character data into the original text.  The trick
- *	here determine the runs of characters that do not require 
- *	replacements and to append the substring in one shot.  
+ *        Convert XML character data into the original text.  The trick
+ *        here determine the runs of characters that do not require 
+ *        replacements and to append the substring in one shot.  
  */
 const char*
 EntityRef::decode (const char* string, unsigned int len)
@@ -68,36 +68,36 @@ EntityRef::decode (const char* string, unsigned int len)
         len = strlen(string);
     }
     const char *p, *start, *pend;
-    start = string;			/* Mark the start of a run of
-					 * characters that contain no
-					 * replacements. */
+    start = string;                     /* Mark the start of a run of
+                                         * characters that contain no
+                                         * replacements. */
     for (p = string, pend = p + len; p < pend; /*empty*/) {
-	if (*p == '&') {
-	    PredefEntityRef *ep, *epend;
-	    for (ep = predef, epend = ep + nPredefs; ep < epend; ep++) {
-		size_t length;
-		length = pend - p;	/* Get the # bytes left. */
-		if ((length >= ep->length) && (ep->replacement[1] == *(p+1)) && 
-		    (strncmp(ep->replacement, p, ep->length) == 0)) {
-		    /* Found entity replacement. Append any preceding
-		     * characters into the buffer before the entity itself. */
-		    if (p > start) {
-			_bout.append(start, p - start);
-			start = p + ep->length;
-		    }
-		    _bout.append(ep->entity, 1);
-		    p += ep->length;
-		    goto next;
-		} 
-	    }
-	}
-	p++;
+        if (*p == '&') {
+            PredefEntityRef *ep, *epend;
+            for (ep = predef, epend = ep + nPredefs; ep < epend; ep++) {
+                size_t length;
+                length = pend - p;        /* Get the # bytes left. */
+                if ((length >= ep->length) && (ep->replacement[1] == *(p+1)) &&
+                    (strncmp(ep->replacement, p, ep->length) == 0)) {
+                    /* Found entity replacement. Append any preceding
+                     * characters into the buffer before the entity itself. */
+                    if (p > start) {
+                        _bout.append(start, p - start);
+                    }
+                    start = p + ep->length;
+                    _bout.append(ep->entity, 1);
+                    p += ep->length;
+                    goto next;
+                }
+            }
+        }
+        p++;
     next:
-	;
+        ;
     }
     if (p > start) {
-	/* Append any left over characters into the buffer. */
-	_bout.append(start, p - start);
+        /* Append any left over characters into the buffer. */
+        _bout.append(start, p - start);
     }
     _bout.append("\0", 1);
     return _bout.bytes();
@@ -107,35 +107,35 @@ const char*
 EntityRef::encode (const char* string, unsigned int len)
 {
     if (string == NULL) {
-        return NULL;		       /* Don't do anything with NULL
-					* strings. */
+        return NULL;                   /* Don't do anything with NULL
+                                        * strings. */
     }
     _bout.clear();
     if (len == 0) {
         len = strlen(string);
     }
     const char *p, *start, *pend;
-    start = string;			/* Mark the start of a run of
-					 * characters that contain no
-					 * replacements. */
+    start = string;                     /* Mark the start of a run of
+                                         * characters that contain no
+                                         * replacements. */
     for (p = string, pend = p + len; p < pend; p++) {
-	PredefEntityRef *ep, *epend;
-	for (ep = predef, epend = ep + nPredefs; ep < epend; ep++) {
-	    if (ep->entity[0] == *p) {
-		/* Found entity requiring replacement. Append any preceding
-		 * characters into the buffer before the entity itself. */
-		if (p > start) {
-		    _bout.append(start, p - start);
-		    start = p + 1;
-		}
-		_bout.append(ep->replacement, ep->length);
-		break;
-	    }
-	}
+        PredefEntityRef *ep, *epend;
+        for (ep = predef, epend = ep + nPredefs; ep < epend; ep++) {
+            if (ep->entity[0] == *p) {
+                /* Found entity requiring replacement. Append any preceding
+                 * characters into the buffer before the entity itself. */
+                if (p > start) {
+                    _bout.append(start, p - start);
+                }
+                start = p + 1;
+                _bout.append(ep->replacement, ep->length);
+                break;
+            }
+        }
     }
     if (p > start) {
-	/* Append any left over characters into the buffer. */
-	_bout.append(start, p - start);
+        /* Append any left over characters into the buffer. */
+        _bout.append(start, p - start);
     }
     _bout.append("\0", 1);
     return _bout.bytes();
