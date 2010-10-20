c26f51e45dd7d5e7d4610c7a14f41bfba1dfd2b1
dkearney <dkearney@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2010-10-20 01:18:44 +0000

    fixed layout issues with minordial canvas. added -minortick and -majortick options to control how tick marks were layed out. updated marker placement estimation code to handle placement of marker when a tick for a frame does not exist. added current marker flag. updated dialdemo test code

--- trunk/examples/video/dialdemo.tcl
+++ trunk/examples/video/dialdemo.tcl
@@ -4,6 +4,8 @@ exec wish "$0" $*
 
 package require RapptureGUI
 
+#wm geometry . 600x300
+
 set w 0
 set t 0
 set min 0
@@ -34,33 +36,46 @@ Rappture::Videodial .f.d3 \
     -activelinecolor "red" \
     -min $min\
     -max $max \
+    -minortick 20 \
+    -majortick 100 \
     -knobimage "" \
-    -knobposition ne@bottom
+    -knobposition center@bottom
 
 #pack .f.d1 -expand yes -fill both
 pack .f.d3 -expand yes -fill both
 
 after 200 {
     update idletasks
-#    .f.d3 mark start 5
-#    .f.d3 mark end 10
-#    .f.d3 mark particle0 17
-#    .f.d3 mark particle1 10
-#    .f.d3 mark particle2 4
-#    .f.d3 mark particle3 54
-#    .f.d3 mark particle2 33
-#    .f.d3 mark arrow 17
-    .f.d3 bball
+    .f.d3 mark start 5
+    .f.d3 mark end 76
+    .f.d3 mark particle0 17
+    .f.d3 mark particle1 10
+    .f.d3 mark particle2 4
+    .f.d3 mark particle3 54
+    .f.d3 mark particle2 33
+    .f.d3 mark particle1000 1001
+    .f.d3 mark particle1100 1101
+    .f.d3 mark particle1200 1201
+    .f.d3 mark particle1300 1301
+    .f.d3 mark particle1400 1401
+    .f.d3 mark particle1500 1501
+    .f.d3 mark particle1600 1601
+    .f.d3 mark particle1700 1701
+    .f.d3 mark particle1800 1801
+    .f.d3 mark particle1900 1901
+    .f.d3 mark particle2000 2001
+    .f.d3 mark arrow 17
+#    .f.d3 bball
 }
 
 
-set t 6000
-#after idle play
+set t 0
+# after 500 play
 proc play {} {
     global t min max
-    if {($t+1) < 7000} {
+    if {($t+1) <= $max} {
         incr t
         .f.d3 current $t
-        after 8 play
+        after 10 play
     }
 }
