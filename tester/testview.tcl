174eb03d14ecf790381b5a6b26679cef7d479f01
braffert <braffert@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2010-12-24 17:15:21 +0000

    Regression tester: adding Test class to keep testing logic isolated from GUI

--- tester/testview.tcl
+++ tester/testview.tcl
@@ -1,10 +1,13 @@
 # ----------------------------------------------------------------------
-# COMPONENT: testview - display the results of a test
+#  COMPONENT: testview - display the results of a test
 #
-# Entire right hand side of the regression tester.  Contains a small
-# text widget, plus two TestAnalyzer widgets.  The analyzers are used to
-# show the golden set of results, and the new results if the test has
-# been ran.  Constructor requires a tool.xml for the tool being tested.
+#  Entire right hand side of the regression tester.  Contains a text
+#  widget for the test description, plus two TestAnalyzer widgets.  The
+#  analyzers are used to show the golden results, and to compare them to
+#  the new results if the test has been ran.  The new version's tool.xml
+#  must be provided through the -toolxml configuration option, and the
+#  -test configuration option is used to provide a Test object to
+#  display.
 # ======================================================================
 #  AUTHOR:  Ben Rafferty, Purdue University
 #  Copyright (c) 2010  Purdue Research Foundation
@@ -29,27 +32,25 @@ option add *TestView.boldTextFont \
 itcl::class Rappture::Tester::TestView {
     inherit itk::Widget 
 
-    protected method updateAnalyzer {args}
-    protected method updateResult {runfile}
-    protected method showStatus {text}
-    protected method showDescription {text}
-    public method update {datapairs}
-    public method reset 
-
-    public variable data
-    protected variable _toolobj
+    public variable test 
+    public variable toolxml
+    protected variable _tool
 
     constructor {toolxml args} { #defined later }
+
+    protected method reset 
+    protected method showDescription {text}
+    protected method showStatus {text}
+    protected method updateAnalyzer {args}
+    protected method updateResult {runfile}
+
 }
 
 # ----------------------------------------------------------------------
 # CONSTRUCTOR
 # ----------------------------------------------------------------------
-itcl::body Rappture::Tester::TestView::constructor {toolxml args} {
-   
-    set _toolobj [Rappture::Tool ::#auto [Rappture::library $toolxml] \
-         [file dirname $toolxml]]
- 
+itcl::body Rappture::Tester::TestView::constructor {args} {
+
     itk_component add status {
         label $itk_interior.status
     }
@@ -74,60 +75,76 @@ itcl::body Rappture::Tester::TestView::constructor {toolxml args} {
     } {
     }
     $itk_component(tabs) insert end "Analyzer" -ipady 25 -fill both
-    $itk_component(tabs) insert end "Result" -ipady 25 -fill both \
+    $itk_component(tabs) insert end "Info" -ipady 25 -fill both \
         -state disabled
 
-    itk_component add analyzer {
-        Rappture::Tester::TestAnalyzer $itk_component(tabs).analyzer $_toolobj
-    }
-    $itk_component(tabs) tab configure "Analyzer" \
-        -window $itk_component(tabs).analyzer
+    #itk_component add analyzer {
+    #    Rappture::Tester::TestAnalyzer $itk_component(tabs).analyzer $_tool
+    #}
+    #$itk_component(tabs) tab configure "Analyzer" \
+    #    -window $itk_component(tabs).analyzer
 
-    itk_component add result {
-        text $itk_component(tabs).result
+    itk_component add info {
+        text $itk_component(tabs).info
     }
-    $itk_component(tabs) tab configure "Result" -window $itk_component(result)
+    $itk_component(tabs) tab configure "Info" -window $itk_component(info)
     pack $itk_component(tabs) -expand yes -fill both -side top
 
     eval itk_initialize $args
+
+    if {$toolxml == "" } {
+        error "no -toolxml configuration option given."
+    }
+}
+
+# --------------------------------------------------------------------
+# Create a new Tool object if -toolxml option is changed.  Also
+# refresh the display by calling the configbody for the -test option.
+# ----------------------------------------------------------------------
+itcl::configbody Rappture::Tester::TestView::toolxml {
+    set _tool [Rappture::Tool ::#auto [Rappture::library $toolxml] \
+         [file dirname $toolxml]]
+    $this configure -test [$this cget -test]
 }
 
 # ----------------------------------------------------------------------
-# When the -data configuration option is modified, update the display
-# accordingly.  The data passed in should be a list of key value pairs
-# from the TestTree widget.
+# When the -test configuration option is modified, update the display
+# accordingly.  The data passed in should be a Test object, or the
+# empty string to clear the display.
 # ----------------------------------------------------------------------
-itcl::configbody Rappture::Tester::TestView::data {
-    array set darray $data
+itcl::configbody Rappture::Tester::TestView::test {
     # Data array is empty for branch nodes.
-    if {[array names darray] != ""} {
-        if {$darray(ran)} {
-            switch $darray(result) {
+    if {$test != ""} {
+        if {![$test isa Test]} {
+            error "-test option must be a Test object.  $test was given."
+        }
+        if {[$test hasRan]} {
+            switch [$test getResult] {
                 Pass {showStatus "Test passed."}
                 Fail {showStatus "Test failed."}
                 Error {showStatus "Error while running test."}
             }
-            if {$darray(runfile) != ""} {
+            if {[$test getRunfile] != ""} {
                 # HACK: Add a new input to differentiate between golden and
                 # test result.  Otherwise, the slider at the bottom won't
                 # be enabled.
-                set golden [Rappture::library $darray(testxml)]
+                set golden [Rappture::library [$test getTestxml]]
                 $golden put input.run.current "Golden"
-                set result [Rappture::library $darray(runfile)]
-                $result put input.run.current "Test"
-                updateAnalyzer $golden $result
-                updateResult $data
+                set runfile [Rappture::library [$test getRunfile]]
+                $runfile put input.run.current "Test result"
+                updateAnalyzer $golden $runfile
+                updateResult $test
             } else {
                 updateResult
             }
         } else {
             showStatus "Test has not yet ran."
             updateResult
-            if {$darray(testxml) != ""} {
-                updateAnalyzer [Rappture::library $darray(testxml)]
+            if {[$test getTestxml] != ""} {
+                updateAnalyzer [Rappture::library [$test getTestxml]]
             }
         }
-        set descr [[Rappture::library $darray(testxml)] get test.description]
+        set descr [[Rappture::library [$test getTestxml]] get test.description]
         if {$descr == ""} {
             set descr "No description."
         }
@@ -155,72 +172,6 @@ itcl::body Rappture::Tester::TestView::reset {} {
 }
 
 # ----------------------------------------------------------------------
-# USAGE: updateAnalyzer ?<lib> <lib>...?
-#
-# Clears the analyzer and loads the given library objects.  Used to load 
-# both the golden result as well as the test result.  Clears the
-# analyzer space if no arguments are given.
-# HACK: Destroys the existing analyzer widget and creates a new one.  
-# Eventually this should be able to keep the same widget and swap in
-# and out different result sets.
-# ----------------------------------------------------------------------
-itcl::body Rappture::Tester::TestView::updateAnalyzer {args} {
-    catch {
-        $itk_component(analyzer) clear
-        destroy $itk_component(analyzer)
-    }
-    itk_component add analyzer {
-        Rappture::Tester::TestAnalyzer $itk_component(tabs).analyzer $_toolobj
-    }
-    $itk_component(tabs) tab configure "Analyzer" \
-        -window $itk_component(analyzer)
-    foreach lib $args {
-        $itk_component(analyzer) display $lib
-    }
-    
-}
-
-# ----------------------------------------------------------------------
-# USAGE: updateResult ?data?
-#
-# Given a set of key value pairs from the test tree, update the result
-# page of the testview widget.  If no arguments are given, disable the
-# result page.
-# ----------------------------------------------------------------------
-itcl::body Rappture::Tester::TestView::updateResult {args} {
-    if {[llength $args] == 0} {
-        $itk_component(result) delete 0.0 end
-        # TODO: Switch back to analyzer tab.  Why doesn't this work?
-        $itk_component(tabs) invoke \
-            [$itk_component(tabs) index -name "Analyzer"]
-        $itk_component(tabs) tab configure "Result" -state disabled
-        return
-    } elseif {[llength $args] == 1} {
-        array set darray [lindex $args 0]
-        $itk_component(tabs) tab configure "Result" -state normal
-        $itk_component(result) delete 0.0 end
-        $itk_component(result) insert end "Test xml: $darray(testxml)\n"
-        $itk_component(result) insert end "Runfile: $darray(runfile)\n"
-        if {$darray(result) == "Fail"} {
-            $itk_component(result) insert end "Diffs: $darray(diffs)\n" 
-            $itk_component(result) insert end "Missing: $darray(missing)\n"
-            $itk_component(result) insert end "Added: $darray(added)\n"
-        }
-    } else {
-        error "wrong # args: should be \"updateResult ?data?\""
-    }
-}
-
-# ----------------------------------------------------------------------
-# USAGE: showStatus <text>
-#
-# Displays a string in the status info space at the top of the widget.
-# ----------------------------------------------------------------------
-itcl::body Rappture::Tester::TestView::showStatus {text} {
-    $itk_component(status) configure -text "$text"
-}
-
-# ----------------------------------------------------------------------
 # USAGE: showDescription <text>
 #
 # Displays a string in the description text space near the top of the
@@ -236,6 +187,76 @@ itcl::body Rappture::Tester::TestView::showDescription {text} {
         $itk_component(description) configure -relief flat
     } else {
         $itk_component(description) configure -relief sunken
+    }
+}
+
+# ----------------------------------------------------------------------
+# USAGE: showStatus <text>
+#
+# Displays a string in the status info space at the top of the widget.
+# ----------------------------------------------------------------------
+itcl::body Rappture::Tester::TestView::showStatus {text} {
+    $itk_component(status) configure -text "$text"
+}
+
+# ----------------------------------------------------------------------
+# USAGE: updateAnalyzer ?<lib> <lib>...?
+#
+# Clears the analyzer and loads the given library objects.  Used to load 
+# both the golden result as well as the test result.  Clears the
+# analyzer space if no arguments are given.
+# HACK: Destroys the existing analyzer widget and creates a new one.  
+# Eventually this should be able to keep the same widget and swap in
+# and out different result sets.
+# ----------------------------------------------------------------------
+itcl::body Rappture::Tester::TestView::updateAnalyzer {args} {
+    catch {
+        $itk_component(analyzer) clear
+        destroy $itk_component(analyzer)
+    }
+    itk_component add analyzer {
+        Rappture::Tester::TestAnalyzer $itk_component(tabs).analyzer $_tool
+    }
+    $itk_component(tabs) tab configure "Analyzer" \
+        -window $itk_component(analyzer)
+    foreach lib $args {
+        $itk_component(analyzer) display $lib
+    }
+    
+}
+
+# ----------------------------------------------------------------------
+# USAGE: updateResult ?test?
+#
+# Given a set of key value pairs from the test tree, update the info 
+# page of the testview widget.  If no arguments are given, disable the
+# info page.
+# ----------------------------------------------------------------------
+itcl::body Rappture::Tester::TestView::updateResult {args} {
+    if {[llength $args] == 0} {
+        $itk_component(info) delete 0.0 end
+        # TODO: Switch back to analyzer tab.  Why doesn't this work?
+        $itk_component(tabs) invoke \
+            [$itk_component(tabs) index -name "Analyzer"]
+        $itk_component(tabs) tab configure "Info" -state disabled
+        return
+    } elseif {[llength $args] == 1} {
+        set testxml [$test getTestxml]
+        set runfile [$test getRunfile]
+        $itk_component(tabs) tab configure "Info" -state normal
+        $itk_component(info) delete 0.0 end
+        $itk_component(info) insert end "Test xml: $testxml\n"
+        $itk_component(info) insert end "Runfile: $runfile\n"
+        if {[$test getResult] == "Fail"} {
+            set diffs [$test getDiffs]
+            set missing [$test getMissing]
+            set added [$test getAdded]
+            $itk_component(info) insert end "Diffs: $diffs\n" 
+            $itk_component(info) insert end "Missing: $missing\n"
+            $itk_component(info) insert end "Added: $added\n"
+        }
+    } else {
+        error "wrong # args: should be \"updateResult ?data?\""
     }
 }
 
