8dd4e36f08864262b534b62bb108be51c7618bed
gah <gah@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2011-03-10 15:18:44 +0000

    Added vtkcontourviewer debug tool

--- gui/apps/launcher.tcl
+++ gui/apps/launcher.tcl
@@ -86,6 +86,14 @@ while {[llength $argv] > 0} {
     }
 }
 
-# invoke the main program with the args
-puts stderr alist=$alist
-eval exec wish [list $mainscript] $alist
+# Invoke the main program with the args
+
+# Note: We're sourcing the driver file "main.tcl" rather than exec-ing
+#       wish because we want to see stderr and stdout messages when they
+#	written, rather than when the program completes.  It also eliminates
+#	one process waiting for the other to complete. If "exec" is needed, 
+#	then the following could be replaced with blt::bgexec.  It doesn't
+#	try to redirect stderr into a file.
+set argv $alist
+package require Tk
+source  $mainscript
