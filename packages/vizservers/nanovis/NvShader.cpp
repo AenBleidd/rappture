6fe4057be36fdcd816c9432137471a4d00a4b397
ldelgass <ldelgass@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2012-03-09 20:52:09 +0000

    Formatting and fix loadVertexProgram in NvShader

--- packages/vizservers/nanovis/NvShader.cpp
+++ packages/vizservers/nanovis/NvShader.cpp
@@ -1,5 +1,6 @@
 /* -*- mode: c++; c-basic-offset: 4; indent-tabs-mode: nil -*- */
 #include <stdio.h>
+
 #include "global.h"
 #include "NvShader.h"
 
@@ -10,9 +11,9 @@ void NvInitCG()
     g_context = cgCreateContext();
 }
 
-NvShader::NvShader()
-
-: _cgVP(0), _cgFP(0)
+NvShader::NvShader():
+    _cgVP(0),
+    _cgFP(0)
 {
 }
 
@@ -21,27 +22,25 @@ NvShader::~NvShader()
     resetPrograms();
 }
 
-void NvShader::loadVertexProgram(const char* fileName, const char* entryPoint)
+void NvShader::loadVertexProgram(const char *fileName, const char *entryPoint)
 {
     resetPrograms();
 
-    _cgFP = LoadCgSourceProgram(g_context, fileName, CG_PROFILE_VP30, entryPoint);
+    _cgVP = LoadCgSourceProgram(g_context, fileName, CG_PROFILE_VP30, entryPoint);
 }
 
-void NvShader::loadFragmentProgram(const char* fileName, const char* entryPoint)
+void NvShader::loadFragmentProgram(const char *fileName, const char *entryPoint)
 {
     _cgFP = LoadCgSourceProgram(g_context, fileName, CG_PROFILE_FP30, entryPoint);
 }
 
 void NvShader::resetPrograms()
 {
-    if (_cgVP > 0)
-    {
+    if (_cgVP > 0) {
         cgDestroyProgram(_cgVP);
     }
 
-    if (_cgFP > 0)
-    {
+    if (_cgFP > 0) {
         cgDestroyProgram(_cgFP);
     }
 }
@@ -51,5 +50,3 @@ void NvShader::setErrorCallback(NvCgCallbackFunction callback)
     TRACE("NvShader callback\n");
     cgSetErrorCallback(callback);
 }
-
-
--- packages/vizservers/nanovis/PointShader.h
+++ packages/vizservers/nanovis/PointShader.h
@@ -1,74 +1,68 @@
 /* -*- mode: c++; c-basic-offset: 4; indent-tabs-mode: nil -*- */
-#ifndef __POINTSHADER_H__
-#define __POINTSHADER_H__
+#ifndef POINTSHADER_H
+#define POINTSHADER_H
 
 #include "NvShader.h"
 #include "Texture3D.h"
 
-class PointShader : public NvShader {
-	CGparameter _modelviewVP;
-	CGparameter _projectionVP;
+class PointShader : public NvShader
+{
+public:
+    PointShader();
 
-	CGparameter _attenVP;
-	CGparameter _posoffsetVP;
-	CGparameter _baseposVP;
-	CGparameter _scaleVP;
-	CGparameter _normalParam;
+    ~PointShader();
 
-	Texture3D* _normal;
-public :
-	PointShader();
-	~PointShader();
-protected :
-	virtual void setParameters();
-	virtual void resetParameters();
+    void setScale(float scale)
+    {
+        cgGLSetParameter4f(_scaleVP, scale, 1.0f, 1.0f, 1.0f);
+    }
 
-public :
-	void setScale(float scale);
-	void setNormalTexture(Texture3D* n);
-    void bind();
-    void unbind();
+    void setNormalTexture(Texture3D *n)
+    {
+        _normal = n;
+    }
+
+    void bind()
+    {
+        setParameters();
+
+        if (_cgVP) {
+            cgGLBindProgram(_cgVP);
+            cgGLEnableProfile((CGprofile) CG_PROFILE_VP30);
+        }
+        if (_cgFP) {
+            cgGLBindProgram(_cgFP);
+            cgGLEnableProfile((CGprofile) CG_PROFILE_FP30);
+        }
+    }
+
+    void unbind()
+    {
+        if (_cgVP) {
+            cgGLDisableProfile((CGprofile)CG_PROFILE_VP30);
+        }
+        if (_cgFP) {
+            cgGLDisableProfile((CGprofile)CG_PROFILE_FP30);
+        }
+
+        resetParameters();
+    }
+
+protected:
+    virtual void setParameters();
+    virtual void resetParameters();
+
+private:
+    CGparameter _modelviewVP;
+    CGparameter _projectionVP;
+
+    CGparameter _attenVP;
+    CGparameter _posoffsetVP;
+    CGparameter _baseposVP;
+    CGparameter _scaleVP;
+    CGparameter _normalParam;
+
+    Texture3D *_normal;
 };
 
-inline void PointShader::setNormalTexture(Texture3D* n)
-{
-	_normal = n;
-}
-
-inline void PointShader::setScale(float scale)
-{
-	cgGLSetParameter4f(_scaleVP, scale, 1.0f, 1.0f, 1.0f);
-}
-
-inline void PointShader::bind()
-{
-    setParameters();
-
-    if (_cgVP)
-    {
-        cgGLBindProgram(_cgVP);
-        cgGLEnableProfile((CGprofile) CG_PROFILE_VP30);
-    }
-    if (_cgFP)
-    {
-        cgGLBindProgram(_cgFP);
-        cgGLEnableProfile((CGprofile) CG_PROFILE_FP30);
-    }
-}
-
-inline void PointShader::unbind()
-{
-    if (_cgVP)
-    {
-        cgGLDisableProfile((CGprofile)CG_PROFILE_VP30 );
-    }
-    
-    if (_cgFP)
-    {
-        cgGLDisableProfile((CGprofile)CG_PROFILE_FP30 );
-    }
-    
-    resetParameters();
-}
-
-#endif // __POINTSHADER_H__
+#endif
