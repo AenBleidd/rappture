c4a468838a54ced39a43f2cf4e09a4f9548e30f7
ldelgass <ldelgass@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2012-03-09 19:50:51 +0000

    Formatting fixes

--- packages/vizservers/nanovis/NvFlowVisRenderer.h
+++ packages/vizservers/nanovis/NvFlowVisRenderer.h
@@ -23,36 +23,31 @@
 class NvParticleRenderer;
 
 class NvFlowVisRenderer {
-    enum {
-	MAX_PARTICLE_RENDERER = 4,
-    };
-
-    int _psys_width;
-    int _psys_height;
-    CGcontext context;
-
-    std::map<std::string, NvVectorField*> _vectorFieldMap;
-
-    bool _activated;
-
-    //NvLIC* licRenderer[3];
 public:
     NvFlowVisRenderer(int w, int h, CGcontext context);
+
     ~NvFlowVisRenderer();
 
     void initialize();
+
     void initialize(const std::string& vfName);
+
     void advect();
+
     void reset();
+
     void render();
 
-    void addVectorField(Volume* volPtr, const Vector3& ori, float scaleX, 
-			float scaleY, float scaleZ, float max);
+    void addVectorField(Volume *volPtr, const Vector3& ori, float scaleX, 
+                        float scaleY, float scaleZ, float max);
 
-    void addVectorField(const std::string& vfName, Volume* volPtr, const Vector3& ori, float scaleX, float scaleY, float scaleZ, float max);
+    void addVectorField(const std::string& vfName, Volume *volPtr, const Vector3& ori,
+                        float scaleX, float scaleY, float scaleZ, float max);
+
     void removeVectorField(const std::string& vfName);
 
     void addPlane(const std::string& vfName, const std::string& name);
+
     void removePlane(const std::string& vfName, const std::string& name);
 
     /** 
@@ -65,27 +60,49 @@ public:
      * @param pos : Specify the position of slice
      */
     void setPlanePos(const std::string& vfName, const std::string& name, float pos);
+
     void setParticleColor(const std::string& vfName, const std::string& name, const Vector4& color);
+
     void activatePlane(const std::string& vfName, const std::string& name);
+
     void deactivatePlane(const std::string& vfName, const std::string& name);
 
     void activateVectorField(const std::string& vfName);
+
     void deactivateVectorField(const std::string& vfName);
 
     ////////////////////
     // DEVICE SHAPE
     void addDeviceShape(const std::string& vfName, const std::string& name, const NvDeviceShape& shape);
+
     void removeDeviceShape(const std::string& vfName, const std::string& name);
+
     void activateDeviceShape(const std::string& vfName);
+
     void deactivateDeviceShape(const std::string& vfName);
+
     void activateDeviceShape(const std::string& vfName, const std::string& name);
+
     void deactivateDeviceShape(const std::string& vfName, const std::string& name);
-    bool active(void) {
+
+    bool active()
+    {
 	return _activated;
     }
-    void active(bool state) {
+
+    void active(bool state)
+    {
 	_activated = state;
     }
+
+private:
+    int _psys_width;
+    int _psys_height;
+    CGcontext context;
+
+    std::map<std::string, NvVectorField *> _vectorFieldMap;
+
+    bool _activated;
 };
 
 #endif
--- packages/vizservers/nanovis/PCASplit.h
+++ packages/vizservers/nanovis/PCASplit.h
@@ -1,164 +1,181 @@
 /* -*- mode: c++; c-basic-offset: 4; indent-tabs-mode: nil -*- */
-#ifndef __PCA_SPLIT_H__
-#define __PCA_SPLIT_H__
+#ifndef PCA_SPLIT_H
+#define PCA_SPLIT_H
 
 #include <memory.h>
+
 #include "Vector3.h"
 #include "Vector4.h"
 
 namespace PCA {
 
-class Point {
-public :
-	Vector3 position;
-	// histogram Index;
-	//unsigned char color;
-	Vector4 color;
-	float size;
+class Point
+{
+public:
+    Point() :
+        size(1.0f),
+        value(0.0f)
+    {}
+
+    Vector3 position;
+    // histogram Index;
+    //unsigned char color;
+    Vector4 color;
+    float size;
     float value;
-public :
-	Point() : size(1.0f), value(0.0f) {}
 };
 
+class Cluster
+{
+public:
+    Cluster() :
+        color(1.0f, 1.0f, 1.0f, 1.0f), 
+        scale(0.0f), 
+        numOfChildren(0), 
+        numOfPoints(0),
+        points(0), 
+        children(0), 
+        vbo(0)/*, minValue(0.0f), maxValue(0.0f)*/
+        , level(0)
+    {
+    }
 
+    void setChildren(Cluster *children, int count)
+    {
+        this->children = children;
+        numOfChildren = count;
+    }
+    void setPoints(Point *points, int count)
+    {
+        this->points = points;
+        numOfPoints = count;
+    }
 
-class Cluster {
-public :
-	Vector3 centroid;
-	Vector4 color;
-	float scale;
+    Vector3 centroid;
+    Vector4 color;
+    float scale;
 
-	int numOfChildren;
-	int numOfPoints;
-	Point* points;
-	Cluster* children;
-	unsigned int vbo;
-	int level;
-
-public :
-
-	Cluster() : 
-	    color(1.0f, 1.0f, 1.0f, 1.0f), 
-	    scale(0.0f), 
-	    numOfChildren(0), 
-	    numOfPoints(0),
-	    points(0), 
-	    children(0), 
-	    vbo(0)/*, minValue(0.0f), maxValue(0.0f)*/
-	    , level(0)
-	{
-	}
-
-	void setChildren(Cluster* children, int count)
-	{
-		this->children = children;
-		numOfChildren = count;
-	}
-	void setPoints(Point* points, int count)
-	{
-		this->points = points;
-		numOfPoints = count;
-	}
-	
+    int numOfChildren;
+    int numOfPoints;
+    Point *points;
+    Cluster *children;
+    unsigned int vbo;
+    int level;
 };
 
-class ClusterAccel {
-public :
-	Cluster* root;
-	int maxLevel;
-	Cluster** startPointerCluster;
-	int* numOfClusters;
-	unsigned int* _vbo;
-public :
-	ClusterAccel(int maxlevel) 
-		: root(0), maxLevel(maxlevel)
-	{
-		startPointerCluster = new Cluster*[maxLevel];
-		numOfClusters = new int[maxLevel];
-		_vbo = new unsigned int[maxLevel];
-		memset(_vbo, 0, sizeof(unsigned int) * maxLevel);
-	}
+class ClusterAccel
+{
+public:
+    ClusterAccel(int maxlevel)
+        : root(0),
+          maxLevel(maxlevel)
+    {
+        startPointerCluster = new Cluster *[maxLevel];
+        numOfClusters = new int[maxLevel];
+        _vbo = new unsigned int[maxLevel];
+        memset(_vbo, 0, sizeof(unsigned int) * maxLevel);
+    }
+
+    Cluster *root;
+    int maxLevel;
+    Cluster **startPointerCluster;
+    int *numOfClusters;
+    unsigned int *_vbo;
 };
 
-class Cluster_t {
-public :
-	Vector3 centroid_t;
-	int		numOfPoints_t;
-	float	scale_t;
-	Point*	points_t;
+class Cluster_t
+{
+public:
+    Cluster_t() :
+        numOfPoints_t(0),
+        scale_t(0.0f),
+        points_t(0)
+    {}
 
-	Cluster_t() : numOfPoints_t(0), scale_t(0.0f), points_t(0) 
-		{}
+    Vector3 centroid_t;
+    int numOfPoints_t;
+    float scale_t;
+    Point *points_t;
 };
 
-class ClusterListNode {
-public :
-	Cluster_t* data;
-	ClusterListNode* next;
-public :
-	ClusterListNode(Cluster_t* d, ClusterListNode* n)
-		: data(d), next(n)
-	{}
+class ClusterListNode
+{
+public:
+    ClusterListNode(Cluster_t *d, ClusterListNode *n) :
+        data(d),
+        next(n)
+    {}
 
-	ClusterListNode()
-		: data(0), next(0)
-	{}
+    ClusterListNode() :
+        data(NULL),
+        next(NULL)
+    {}
+
+    Cluster_t *data;
+    ClusterListNode *next;
 };
 
+class PCASplit
+{
+public:
+    enum {
+        MAX_INDEX = 8000000
+    };
 
-class PCASplit {
-	enum {
-		MAX_INDEX =8000000
-	};
-private :
-	ClusterListNode* _curClusterNode;
-	ClusterListNode* _memClusterChunk1;
-	ClusterListNode* _memClusterChunk2;
-	ClusterListNode* _curMemClusterChunk;
+    PCASplit();
 
-	int _memClusterChunkIndex;
+    ~PCASplit();
 
-	Cluster* _curRoot;
+    ClusterAccel *doIt(Point *data, int count);
 
-	int _curClusterCount;
-	int _maxLevel;
-	float _minDistance;
-	float _distanceScale;
-	unsigned int* _indices;
-	int _indexCount;
-	int _finalMaxLevel;
+    void setMinDistance(float minDistance)
+    {
+        _minDistance = minDistance;
+    }
 
-	ClusterAccel* _clusterHeader;
-public :
-	PCASplit();
-	~PCASplit();
-public :
-	ClusterAccel* doIt(Point* data, int count);
-    void setMinDistance(const float minDistance);
-    void setMaxLevel(int maxLevel);
+    void setMaxLevel(int maxLevel)
+    {
+        _maxLevel = maxLevel;
+    }
 
-private :	
-	void init();
-	void analyze(ClusterListNode* cluster, Cluster* parent, int level, float limit);
-	void addLeafCluster(Cluster_t* cluster);
-	Cluster* createClusterBlock(ClusterListNode* clusterList, int count, int level);
-	void split(Point* data, int count, float maxDistortion);
-	float getMaxDistortionSquare(ClusterListNode* clusterList, int count);
-public :
-	static void computeDistortion(Point* data, int count, const Vector3& mean, float& distortion, float& maxSize);
-	static void computeCentroid(Point* data, int count, Vector3& mean);
-	static void computeCovariant(Point* data, int count, const Vector3& mean, float* m);
+    static void computeDistortion(Point *data, int count, const Vector3& mean, float& distortion, float& maxSize);
+
+    static void computeCentroid(Point *data, int count, Vector3& mean);
+
+    static void computeCovariant(Point *data, int count, const Vector3& mean, float *m);
+
+private:
+    void init();
+
+    void analyze(ClusterListNode *cluster, Cluster *parent, int level, float limit);
+
+    void addLeafCluster(Cluster_t *cluster);
+
+    Cluster *createClusterBlock(ClusterListNode *clusterList, int count, int level);
+
+    void split(Point *data, int count, float maxDistortion);
+
+    float getMaxDistortionSquare(ClusterListNode *clusterList, int count);
+
+    ClusterAccel *_clusterHeader;
+
+    ClusterListNode *_curClusterNode;
+    ClusterListNode *_memClusterChunk1;
+    ClusterListNode *_memClusterChunk2;
+    ClusterListNode *_curMemClusterChunk;
+
+    int _memClusterChunkIndex;
+
+    Cluster *_curRoot;
+
+    int _curClusterCount;
+    int _maxLevel;
+    float _minDistance;
+    float _distanceScale;
+    unsigned int *_indices;
+    int _indexCount;
+    int _finalMaxLevel;
 };	
-
-inline void PCASplit::setMinDistance(const float minDistance)
-{
-    _minDistance = minDistance;
-}
-
-inline void PCASplit::setMaxLevel(int maxLevel)
-{
-    _maxLevel = maxLevel;
-}
 
 }
 
