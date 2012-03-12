fc401985c29690b39ddb1c9140c6b8b29b21f944
ldelgass <ldelgass@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2012-03-12 17:41:32 +0000

    More cleanups

--- packages/vizservers/nanovis/dxReaderCommon.h
+++ packages/vizservers/nanovis/dxReaderCommon.h
@@ -1,11 +1,18 @@
 /* -*- mode: c++; c-basic-offset: 4; indent-tabs-mode: nil -*- */
-#ifndef _DX_READER_COMMON_H
-#define _DX_READER_COMMON_H
+#ifndef DX_READER_COMMON_H
+#define DX_READER_COMMON_H
 
-float* merge(float* scalar, float* gradient, int size);
-void normalizeScalar(float* fdata, int count, float min, float max);
-float* computeGradient(float* fdata, int width, int height, int depth,
-        float min, float max);
-void computeSimpleGradient(float* data, int nx, int ny, int nz);
+extern float *
+merge(float *scalar, float *gradient, int size);
 
-#endif /*_DX_READER_COMMON_H*/
+extern void
+normalizeScalar(float *fdata, int count, float min, float max);
+
+extern float *
+computeGradient(float *fdata, int width, int height, int depth,
+                float min, float max);
+
+extern void
+computeSimpleGradient(float *data, int nx, int ny, int nz);
+
+#endif
