1ae9c6d6476802795948cdfbfcbcd249209c06ad
ldelgass <ldelgass@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2012-03-30 05:55:56 +0000

    janitorial

--- trunk/packages/vizservers/nanovis/transfer-function/ControlPoint.cpp
+++ trunk/packages/vizservers/nanovis/transfer-function/ControlPoint.cpp
@@ -1,9 +1,5 @@
 /* -*- mode: c++; c-basic-offset: 4; indent-tabs-mode: nil -*- */
-/*
- * ----------------------------------------------------------------------
- * ControlPoint.h
- *
- * ======================================================================
+/* ======================================================================
  *  AUTHOR:  Wei Qiao <qiaow@purdue.edu>
  *           Purdue Rendering and Perceptualization Lab (PURPL)
  *
@@ -13,21 +9,19 @@
  *  redistribution of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  * ======================================================================
  */
-
 #include <math.h>
 #include "TransferFunctionMain.h"
 #include "ControlPoint.h"
 
-
 ControlPoint::ControlPoint(double _x, double _y)
 {
-	x = _x;
-	y = _y;
+    x = _x;
+    y = _y;
 
-	selected = false;
-	dragged = false;
-	
-	next = 0;
+    selected = false;
+    dragged = false;
+        
+    next = 0;
 }
 
 ControlPoint::~ControlPoint()
@@ -37,143 +31,128 @@ ControlPoint::~ControlPoint()
 
 void ControlPoint::glDraw()
 {
-	//Here is the code for drawing a vertex
-	double size = 4;
+    //Here is the code for drawing a vertex
+    double size = 4;
 
-	glMatrixMode( GL_MODELVIEW );
-	glPushMatrix();
-	glTranslatef( x, y, 0 );
+    glMatrixMode( GL_MODELVIEW );
+    glPushMatrix();
+    glTranslatef( x, y, 0 );
 
-	glBegin(GL_LINES);
+    glBegin(GL_LINES);
 
-	glVertex2d(-size, -size);
-	glVertex2d(+size, +size);
+    glVertex2d(-size, -size);
+    glVertex2d(+size, +size);
 
-	glVertex2d(-size, +size);
-	glVertex2d(+size, -size);
-	
-	glEnd();
+    glVertex2d(-size, +size);
+    glVertex2d(+size, -size);
+        
+    glEnd();
 
-	//Special code for drawing selected vertex and dragged vertex.
+    //Special code for drawing selected vertex and dragged vertex.
 
-	if (selected && !dragged)
-	{
-		double square = size + 4;
-		
-		glBegin(GL_LINE_LOOP);
-			glColor3d(1,0,0);			
-			glVertex2d(-square, -square);
-			glVertex2d(-square, +square);
-			glVertex2d(+square, +square);
-			glVertex2d(+square, -square);
-			glColor3d(0,0,0);
-		glEnd();
+    if (selected && !dragged) {
+        double square = size + 4;
+                
+        glBegin(GL_LINE_LOOP);
+        glColor3d(1,0,0);                        
+        glVertex2d(-square, -square);
+        glVertex2d(-square, +square);
+        glVertex2d(+square, +square);
+        glVertex2d(+square, -square);
+        glColor3d(0,0,0);
+        glEnd();
 
-	}
-	else if (selected && dragged)
-	{
-		double square = 4*sqrt(size + 4);
-		glBegin(GL_LINE_LOOP);
-			glColor3d(1,0,0);
-			glVertex2d(-square, 0);
-			glVertex2d(0, +square);
-			glVertex2d(square, 0);
-			glVertex2d(0, -square);
-			glColor3d(0,0,0);
-		glEnd();
+    } else if (selected && dragged) {
+        double square = 4*sqrt(size + 4);
+        glBegin(GL_LINE_LOOP);
+        glColor3d(1,0,0);
+        glVertex2d(-square, 0);
+        glVertex2d(0, +square);
+        glVertex2d(square, 0);
+        glVertex2d(0, -square);
+        glColor3d(0,0,0);
+        glEnd();
 
-	}
-	glPopMatrix();
+    }
+    glPopMatrix();
 
 }
-
-
 
 void ControlPoint::glDraw_2()
 {
-	//Here is the code for drawing a vertex
-	double size = 4;
+    //Here is the code for drawing a vertex
+    double size = 4;
 
-	glMatrixMode( GL_MODELVIEW );
-	glPushMatrix();
-	glTranslatef( x, y, 0 );
+    glMatrixMode( GL_MODELVIEW );
+    glPushMatrix();
+    glTranslatef( x, y, 0 );
 
-	glBegin(GL_POLYGON);
-		glVertex2d(-size, 0);
-		glVertex2d(-size, -size);
-		glVertex2d(+size, -size);
-		glVertex2d(+size, 0);	
-	glEnd();
+    glBegin(GL_POLYGON);
+    glVertex2d(-size, 0);
+    glVertex2d(-size, -size);
+    glVertex2d(+size, -size);
+    glVertex2d(+size, 0);        
+    glEnd();
 
-	//Special code for drawing selected vertex and dragged vertex.
+    //Special code for drawing selected vertex and dragged vertex.
 
-	if (selected && !dragged)
-	{
-		double square = size + 4;
-		
-		glBegin(GL_LINE_LOOP);
-			glColor3d(1,0,0);			
-			glVertex2d(-square, -square);
-			glVertex2d(-square, +square);
-			glVertex2d(+square, +square);
-			glVertex2d(+square, -square);
-			glColor3d(0,0,0);
-		glEnd();
-
-	}
-	else if (selected && dragged)
-	{
-		double square = 4*sqrt(size + 4);
-		glBegin(GL_LINE_LOOP);
-			glColor3d(1,0,0);
-			glVertex2d(-square, 0);
-			glVertex2d(0, +square);
-			glVertex2d(square, 0);
-			glVertex2d(0, -square);
-			glColor3d(0,0,0);
-		glEnd();
-
-	}
-	glPopMatrix();
+    if (selected && !dragged) {
+        double square = size + 4;
+                
+        glBegin(GL_LINE_LOOP);
+        glColor3d(1,0,0);                        
+        glVertex2d(-square, -square);
+        glVertex2d(-square, +square);
+        glVertex2d(+square, +square);
+        glVertex2d(+square, -square);
+        glColor3d(0,0,0);
+        glEnd();
+    } else if (selected && dragged) {
+        double square = 4*sqrt(size + 4);
+        glBegin(GL_LINE_LOOP);
+        glColor3d(1,0,0);
+        glVertex2d(-square, 0);
+        glVertex2d(0, +square);
+        glVertex2d(square, 0);
+        glVertex2d(0, -square);
+        glColor3d(0,0,0);
+        glEnd();
+    }
+    glPopMatrix();
 
 }
-
 
 void ControlPoint::glDraw_3()
 {
-	//Here is the code for drawing a vertex
-	double size = 7;
+    //Here is the code for drawing a vertex
+    double size = 7;
 
-	glMatrixMode( GL_MODELVIEW );
-	glPushMatrix();
-	glTranslatef( x, y, 0 );
+    glMatrixMode( GL_MODELVIEW );
+    glPushMatrix();
+    glTranslatef( x, y, 0 );
 
-	glBegin(GL_LINES);
+    glBegin(GL_LINES);
 
-	glVertex2d(-2, 0);
-	glVertex2d(-size, 0);
+    glVertex2d(-2, 0);
+    glVertex2d(-size, 0);
 
-	glVertex2d(2, 0);
-	glVertex2d(size, 0);
-
-
-	glVertex2d(0, -2);
-	glVertex2d(0, -size);
-
-	glVertex2d(0, 2);
-	glVertex2d(0, size);
-
-	glEnd();
+    glVertex2d(2, 0);
+    glVertex2d(size, 0);
 
 
-	glPopMatrix();
+    glVertex2d(0, -2);
+    glVertex2d(0, -size);
 
+    glVertex2d(0, 2);
+    glVertex2d(0, size);
+
+    glEnd();
+
+    glPopMatrix();
 }
-
-
 
 void ControlPoint::Set(double x, double y)
 {
-	this->x = x;
-	this->y = y;
+    this->x = x;
+    this->y = y;
 }
--- trunk/packages/vizservers/nanovis/transfer-function/MainWindow.cpp
+++ trunk/packages/vizservers/nanovis/transfer-function/MainWindow.cpp
@@ -1,9 +1,5 @@
 /* -*- mode: c++; c-basic-offset: 4; indent-tabs-mode: nil -*- */
-/*
- * ----------------------------------------------------------------------
- * MainWindow.h: implementation of the main transfer function gui window
- *
- * ======================================================================
+/* ======================================================================
  *  AUTHOR:  Wei Qiao <qiaow@purdue.edu>
  *           Purdue Rendering and Perceptualization Lab (PURPL)
  *
@@ -34,187 +30,173 @@ double distanceThreshold;
 MainTransferFunctionWindow::MainTransferFunctionWindow(){}
 MainTransferFunctionWindow::~MainTransferFunctionWindow(){}
 
-
 //initialize 
-void MainTransferFunctionWindow::mainInit(){
-	
-	distanceThreshold=36;
+void MainTransferFunctionWindow::mainInit()
+{
+    distanceThreshold=36;
 
-	main_winx=600;
-	main_winy=300;
-	
-	glutInitWindowPosition(100, 100);
-	glutInitWindowSize(main_winx, main_winy );
-  
-	mainWindow=glutCreateWindow("Advanced Transfer Function");
+    main_winx=600;
+    main_winy=300;
 
-	glutDisplayFunc(mainDisplay);
-	glutReshapeFunc(mainReshape);
-	glutKeyboardFunc (mainKeyboard);
-	//glutIdleFunc (mainIdle);
+    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(main_winx, main_winy );
+
+    mainWindow=glutCreateWindow("Advanced Transfer Function");
+
+    glutDisplayFunc(mainDisplay);
+    glutReshapeFunc(mainReshape);
+    glutKeyboardFunc (mainKeyboard);
+    //glutIdleFunc (mainIdle);
 
 
-	//glutMouseFunc (mainMouse);
-	//glutMotionFunc (mainMotion);
+    //glutMouseFunc (mainMouse);
+    //glutMotionFunc (mainMotion);
 
-	TransferFunctionGLUTWindow::tfInit(main_winx, main_winy);
-	transferFunctionWindow=glutCreateSubWindow(mainWindow, 0, 0, tf_winx, tf_winy);
-	glutDisplayFunc(TransferFunctionGLUTWindow::tfDisplay);
-	glutReshapeFunc(TransferFunctionGLUTWindow::tfReshape);
-	glutMouseFunc(TransferFunctionGLUTWindow::tfMouse);
-	glutKeyboardFunc(TransferFunctionGLUTWindow::tfKeyboard);
-	glutMotionFunc (TransferFunctionGLUTWindow::tfMotion);
+    TransferFunctionGLUTWindow::tfInit(main_winx, main_winy);
+    transferFunctionWindow=glutCreateSubWindow(mainWindow, 0, 0, tf_winx, tf_winy);
+    glutDisplayFunc(TransferFunctionGLUTWindow::tfDisplay);
+    glutReshapeFunc(TransferFunctionGLUTWindow::tfReshape);
+    glutMouseFunc(TransferFunctionGLUTWindow::tfMouse);
+    glutKeyboardFunc(TransferFunctionGLUTWindow::tfKeyboard);
+    glutMotionFunc (TransferFunctionGLUTWindow::tfMotion);
 
-	ColorGradientGLUTWindow::cmInit(main_winx, main_winy);
-	colorMapWindow=glutCreateSubWindow(mainWindow, 0, main_winy-cm_winy, cm_winx, cm_winy);
-	//ColorGradientGLUTWindow::createGLUIWidgets();	
-	glutDisplayFunc(ColorGradientGLUTWindow::cmDisplay);
-	glutReshapeFunc(ColorGradientGLUTWindow::cmReshape);
-	glutMouseFunc(ColorGradientGLUTWindow::cmMouse);
-	glutMotionFunc(ColorGradientGLUTWindow::cmMotion);
-	glutKeyboardFunc(ColorGradientGLUTWindow::cmKeyboard);
-	
-	GLUI_Master.set_glutIdleFunc(mainIdle);
-	
-	ColorPalette::cpInit();
+    ColorGradientGLUTWindow::cmInit(main_winx, main_winy);
+    colorMapWindow=glutCreateSubWindow(mainWindow, 0, main_winy-cm_winy, cm_winx, cm_winy);
+    //ColorGradientGLUTWindow::createGLUIWidgets();        
+    glutDisplayFunc(ColorGradientGLUTWindow::cmDisplay);
+    glutReshapeFunc(ColorGradientGLUTWindow::cmReshape);
+    glutMouseFunc(ColorGradientGLUTWindow::cmMouse);
+    glutMotionFunc(ColorGradientGLUTWindow::cmMotion);
+    glutKeyboardFunc(ColorGradientGLUTWindow::cmKeyboard);
 
-	//int tmp= glutGetWindow();
-	
-	//load a initial trasfer function
-	//for bluntfin dataset
-	//MainTransferFunctionWindow::loadFile("bluntfin_tf_1");
-	//glutSetWindow(tmp);
+    GLUI_Master.set_glutIdleFunc(mainIdle);
+
+    ColorPalette::cpInit();
+
+    //int tmp= glutGetWindow();
+
+    //load a initial trasfer function
+    //for bluntfin dataset
+    //MainTransferFunctionWindow::loadFile("bluntfin_tf_1");
+    //glutSetWindow(tmp);
 }
 
-
-void MainTransferFunctionWindow::mainDisplay(){
-	glutSetWindow(transferFunctionWindow);
-	glutPostRedisplay();
-	glutSetWindow(colorMapWindow);
-	glutPostRedisplay();
-	glutSetWindow(colorPaletteWindow);
-	glutPostRedisplay();
+void MainTransferFunctionWindow::mainDisplay()
+{
+    glutSetWindow(transferFunctionWindow);
+    glutPostRedisplay();
+    glutSetWindow(colorMapWindow);
+    glutPostRedisplay();
+    glutSetWindow(colorPaletteWindow);
+    glutPostRedisplay();
 }
 
-
-
-void MainTransferFunctionWindow::mainMouse(int button, int state, int x, int y){
-	fprintf(stderr, "main mouse\n");
-	//find out what area received the event
-	if (y<=main_winy-tf_winy){
-		//transfer function received the event
-		glutSetWindow(transferFunctionWindow);
-		TransferFunctionGLUTWindow::tfMouse(button, state, x, y);
-	}
+void MainTransferFunctionWindow::mainMouse(int button, int state, int x, int y)
+{
+    fprintf(stderr, "main mouse\n");
+    //find out what area received the event
+    if (y<=main_winy-tf_winy){
+        //transfer function received the event
+        glutSetWindow(transferFunctionWindow);
+        TransferFunctionGLUTWindow::tfMouse(button, state, x, y);
+    }
 }
 
-
-
-
-
-
-void MainTransferFunctionWindow::mainMotion(int x, int y){
-
-	////////////////////////////////////////
-	//glutSetWindow(transferFunctionWindow);
-	//TransferFunctionGLUTWindow::tfMotion(x, y);
-	/////////////////////////////////////
-
+void MainTransferFunctionWindow::mainMotion(int x, int y)
+{
+    ////////////////////////////////////////
+    //glutSetWindow(transferFunctionWindow);
+    //TransferFunctionGLUTWindow::tfMotion(x, y);
+    /////////////////////////////////////
 }
 
-
-
-void MainTransferFunctionWindow::mainIdle(){
-/*
-	glutSetWindow(mainWindow);
-	glutPostRedisplay();
-	glutSetWindow(transferFunctionWindow);
-	glutPostRedisplay();
-	glutSetWindow(colorMapWindow);
-	glutPostRedisplay();
-	glutSetWindow(colorPaletteWindow);
-	glutPostRedisplay();
-*/
+void MainTransferFunctionWindow::mainIdle()
+{
+    /*
+      glutSetWindow(mainWindow);
+      glutPostRedisplay();
+      glutSetWindow(transferFunctionWindow);
+      glutPostRedisplay();
+      glutSetWindow(colorMapWindow);
+      glutPostRedisplay();
+      glutSetWindow(colorPaletteWindow);
+      glutPostRedisplay();
+    */
 }
 
-
-
-void MainTransferFunctionWindow::mainKeyboard(unsigned char key, int x, int y){
-	switch(key) 
-	{    
+void MainTransferFunctionWindow::mainKeyboard(unsigned char key, int x, int y)
+{
+    switch(key) {    
     case 'q':
-		exit(0);
-		break;    
+        exit(0);
+        break;    
     case 'a':
-		glutSetWindow(transferFunctionWindow);
-		tf_pointEditState=1;
-		break;
+        glutSetWindow(transferFunctionWindow);
+        tf_pointEditState=1;
+        break;
     case 'd':
-		glutSetWindow(transferFunctionWindow);
-		tf_pointEditState=2;
-		if (tf_gvSelectedPoint!=NULL){
-			TransferFunctionGLUTWindow::removePoint(tf_gvSelectedPoint);
-			tf_gvSelectedPoint=0;
-			TransferFunctionGLUTWindow::WriteControlPoints();
-			glutPostRedisplay();
-		}
-		tf_pointEditState=0;
-		break;   
-	case 'p':
-		TransferFunctionGLUTWindow::printPoints();
-		break;
-	case 'w':
-		TransferFunctionGLUTWindow::printInterpolation();
-		break; 
-	case 'r':
-		TransferFunctionGLUTWindow::readHist();
-		break; 
-	case 'u':
-		TransferFunctionGLUTWindow::dumpHist();
-		break; 
-	case 'i':
-		TransferFunctionGLUTWindow::printHist();
-		break;
-
-	default:
-		break;
+        glutSetWindow(transferFunctionWindow);
+        tf_pointEditState=2;
+        if (tf_gvSelectedPoint!=NULL) {
+            TransferFunctionGLUTWindow::removePoint(tf_gvSelectedPoint);
+            tf_gvSelectedPoint=0;
+            TransferFunctionGLUTWindow::WriteControlPoints();
+            glutPostRedisplay();
+        }
+        tf_pointEditState=0;
+        break;   
+    case 'p':
+        TransferFunctionGLUTWindow::printPoints();
+        break;
+    case 'w':
+        TransferFunctionGLUTWindow::printInterpolation();
+        break; 
+    case 'r':
+        TransferFunctionGLUTWindow::readHist();
+        break; 
+    case 'u':
+        TransferFunctionGLUTWindow::dumpHist();
+        break; 
+    case 'i':
+        TransferFunctionGLUTWindow::printHist();
+        break;
+    default:
+        break;
     }  
-
 }
 
-
-void MainTransferFunctionWindow::mainDestroy(){}
-
-
-void MainTransferFunctionWindow::mainReshape(int x, int y){
-
-	if (x==0 || y==0)
-		return;
-
-	main_winx=x;
-	main_winy=y;
-
-	//change size and location of the subwindow
-	cm_winx=main_winx;
-	//cm_winy=main_winy/3;
-	cm_winy=100;
-
-	glutSetWindow(colorMapWindow);
-	glutPositionWindow(0, main_winy-cm_winy);
-	glutReshapeWindow(cm_winx, cm_winy);
-	glutPostRedisplay();
-
-	//change size and location of the subwindow
-	tf_winx=main_winx;
-	//tf_winy=main_winy*2/3;
-	tf_winy=main_winy-100;
-
-	glutSetWindow(transferFunctionWindow);
-	glutPositionWindow(0,0);
-	glutReshapeWindow(tf_winx, tf_winy);
-	glutPostRedisplay();
+void MainTransferFunctionWindow::mainDestroy()
+{
 }
 
+void MainTransferFunctionWindow::mainReshape(int x, int y)
+{
+    if (x==0 || y==0)
+        return;
+
+    main_winx=x;
+    main_winy=y;
+
+    //change size and location of the subwindow
+    cm_winx=main_winx;
+    //cm_winy=main_winy/3;
+    cm_winy=100;
+
+    glutSetWindow(colorMapWindow);
+    glutPositionWindow(0, main_winy-cm_winy);
+    glutReshapeWindow(cm_winx, cm_winy);
+    glutPostRedisplay();
+
+    //change size and location of the subwindow
+    tf_winx=main_winx;
+    //tf_winy=main_winy*2/3;
+    tf_winy=main_winy-100;
+
+    glutSetWindow(transferFunctionWindow);
+    glutPositionWindow(0,0);
+    glutReshapeWindow(tf_winx, tf_winy);
+    glutPostRedisplay();
+}
 
 //////////////////////////////////////////////////////////
 //FILE STRUCTURE:
@@ -237,212 +219,198 @@ void MainTransferFunctionWindow::mainReshape(int x, int y){
 //"END" signals the end of file
 //////////////////////////////////////////////////////////// 
 
+void MainTransferFunctionWindow::loadFile(char* fileName)
+{
+    if (fileName==0 || strlen(fileName)<1) {
+        fprintf(stderr, "Error: file name not supplied.\n");
+        return;
+    }
 
-void MainTransferFunctionWindow::loadFile(char* fileName){
+    FILE* fp=fopen(fileName, "r");
 
-	
-	if (fileName==0 || strlen(fileName)<1){
-		fprintf(stderr, "Error: file name not supplied.\n");
-		return;
-	}
+    if (!fp) {
+        fprintf(stderr, "Error: open file.\n");
+        return;
+    }
 
-	FILE* fp=fopen(fileName, "r");
+    fprintf(stderr, "File \"%s\" opened.\n", fileName);
 
-	if(!fp){
-		fprintf(stderr, "Error: open file.\n");
-		return;
-	}
+    char buf[300];                        //buffer to store one line from file
+    char* token;                        //pointer to token in the buffer
+    char seps[]=" \n";                //seperators
 
-	fprintf(stderr, "File \"%s\" opened.\n", fileName);
+    int num_of_control_point;
+    int num_of_color_point;
+    double control_points[200];
+    double color_points[100];
+    double colors[300];
 
-	char buf[300];			//buffer to store one line from file
-	char* token;			//pointer to token in the buffer
-	char seps[]=" \n";		//seperators
+    //clean up old control points
+    TransferFunctionGLUTWindow::cleanUpPoints();
 
+    fgets(buf, 100, fp);
+    token=strtok(buf, seps);
+    num_of_control_point=(int)atof(token);
 
-	int num_of_control_point;
-	int num_of_color_point;
-	double control_points[200];
-	double color_points[100];
-	double colors[300];
+    //read control points
+    for(int i=0; i<num_of_control_point; i++){
+        fgets(buf, 100, fp);
+        token=strtok(buf, seps);
+        control_points[2*i]=atof(token)*tf_unitWidth+15;
+                
+        token=strtok(NULL, seps);
+        control_points[2*i+1]=(atof(token)*(tf_winy-15))+15;
+                
+        //add points
+        TransferFunctionGLUTWindow::addPoint(control_points[2*i], control_points[2*i+1]);
+    }
+    //rewrite the interpolatoin
+    TransferFunctionGLUTWindow::WriteControlPoints();
+        
+    fgets(buf, 100, fp); //empty line
 
+    fgets(buf, 100, fp);
+    token=strtok(buf, seps);
+    num_of_color_point=(int)atof(token);
 
-	//clean up old control points
-	TransferFunctionGLUTWindow::cleanUpPoints();
+    //read color points
+    for(int i=0; i<num_of_color_point; i++){
+        fgets(buf, 100, fp);
+        token=strtok(buf, seps);
+        color_points[i]=atof(token)*cm_unitWidth+15;
+    }
 
-	fgets(buf, 100, fp);
-	token=strtok(buf, seps);
-	num_of_control_point=(int)atof(token);
+    //clean map
+    map->cleanUp();
 
-	//read control points
-	for(int i=0; i<num_of_control_point; i++){
-		fgets(buf, 100, fp);
-		token=strtok(buf, seps);
-		control_points[2*i]=atof(token)*tf_unitWidth+15;
-		
-		token=strtok(NULL, seps);
-		control_points[2*i+1]=(atof(token)*(tf_winy-15))+15;
-		
-		//add points
-		TransferFunctionGLUTWindow::addPoint(control_points[2*i], control_points[2*i+1]);
-	}
-	//rewrite the interpolatoin
-	TransferFunctionGLUTWindow::WriteControlPoints();
-	
-	fgets(buf, 100, fp); //empty line
+    fgets(buf, 100, fp); //empty line
 
+    //read colors
+    for(int i=0; i<num_of_color_point; i++){
+        fgets(buf, 100, fp);
+        token=strtok(buf, seps);
+        colors[3*i]=atof(token);
+                
+        token=strtok(NULL, seps);
+        colors[3*i+1]=atof(token);
 
-	fgets(buf, 100, fp);
-	token=strtok(buf, seps);
-	num_of_color_point=(int)atof(token);
+        token=strtok(NULL, seps);
+        colors[3*i+2]=atof(token);
 
+        map->addKey(color_points[i], new Color(colors[3*i], colors[3*i+1], colors[3*i+2]));
 
-	//read color points
-	for(int i=0; i<num_of_color_point; i++){
-		fgets(buf, 100, fp);
-		token=strtok(buf, seps);
-		color_points[i]=atof(token)*cm_unitWidth+15;
-	}
+    }
+    //rewrite the interpolatoin
+    ColorGradientGLUTWindow::WriteControlPoints();
 
+    glutSetWindow(transferFunctionWindow);
+    TransferFunctionGLUTWindow::tfDisplay();
 
-	//clean map
-	map->cleanUp();
+    //read modelview settings
+    fgets(buf, 100, fp); //empty line
+        
+    //camero location
+    fgets(buf, 100, fp);
+    token=strtok(buf, seps);
+    //eye_shift_x=atof(token);
+        
+    token=strtok(NULL, seps);
+    //eye_shift_y=atof(token);
 
-	fgets(buf, 100, fp); //empty line
+    token=strtok(NULL, seps);
+    //eyedistance=atof(token);
+        
+    fgets(buf, 100, fp); //empty line
+        
+    //object center location
+    fgets(buf, 100, fp);
+    token=strtok(buf, seps);
+    //obj_center[0]=atof(token);
+        
+    token=strtok(NULL, seps);
+    //obj_center[1]=atof(token);
 
-	//read colors
-	for(int i=0; i<num_of_color_point; i++){
-		fgets(buf, 100, fp);
-		token=strtok(buf, seps);
-		colors[3*i]=atof(token);
-		
-		token=strtok(NULL, seps);
-		colors[3*i+1]=atof(token);
+    token=strtok(NULL, seps);
+    //obj_center[2]=atof(token);
 
-		token=strtok(NULL, seps);
-		colors[3*i+2]=atof(token);
+    fgets(buf, 100, fp); //empty line
+        
+    //scale and field_of_view
+    fgets(buf, 100, fp);
+    token=strtok(buf, seps);
+    control_point_scale=atof(token);
 
-		map->addKey(color_points[i], new Color(colors[3*i], colors[3*i+1], colors[3*i+2]));
+    token=strtok(NULL, seps);
+    //fovy=atof(token);
 
-	}
-	//rewrite the interpolatoin
-	ColorGradientGLUTWindow::WriteControlPoints();
+    if (colorPaletteWindow!=-1) {
+        glutSetWindow(colorPaletteWindow);
+        ColorPalette::cpSyncLive();
+    }
 
-	glutSetWindow(transferFunctionWindow);
-	TransferFunctionGLUTWindow::tfDisplay();
+    fclose(fp);
+        
+    //update transfer function values
+    if (transferFunctionWindow!=-1) {
+        TransferFunctionGLUTWindow::WriteControlPoints();
+    }
 
-	//read modelview settings
-	fgets(buf, 100, fp); //empty line
-	
-	//camero location
-	fgets(buf, 100, fp);
-	token=strtok(buf, seps);
-	//eye_shift_x=atof(token);
-	
-	token=strtok(NULL, seps);
-	//eye_shift_y=atof(token);
-
-	token=strtok(NULL, seps);
-	//eyedistance=atof(token);
-
-	
-	fgets(buf, 100, fp); //empty line
-	
-	//object center location
-	fgets(buf, 100, fp);
-	token=strtok(buf, seps);
-	//obj_center[0]=atof(token);
-	
-	token=strtok(NULL, seps);
-	//obj_center[1]=atof(token);
-
-	token=strtok(NULL, seps);
-	//obj_center[2]=atof(token);
-
-
-	fgets(buf, 100, fp); //empty line
-	
-	//scale and field_of_view
-	fgets(buf, 100, fp);
-	token=strtok(buf, seps);
-	control_point_scale=atof(token);
-	
-	token=strtok(NULL, seps);
-	//fovy=atof(token);
-	
-	if (colorPaletteWindow!=-1){
-		glutSetWindow(colorPaletteWindow);
-		ColorPalette::cpSyncLive();
-	}
-		
-	fclose(fp);
-	
-	//update transfer function values
-	if(transferFunctionWindow!=-1){
-		TransferFunctionGLUTWindow::WriteControlPoints();
-	}
-
-	//render the volume again.
-	//glutSetWindow(render_window);
-	glutPostRedisplay();
+    //render the volume again.
+    //glutSetWindow(render_window);
+    glutPostRedisplay();
 }
 
+void MainTransferFunctionWindow::saveFile(char* fileName)
+{
+    // Check for bad param
+    if (fileName == NULL)
+        return;
+    if (strlen(fileName) < 1)
+        return;
 
+    FILE* fp = fopen(fileName, "w");
 
+    if (!fp)
+        return; //error
 
-void MainTransferFunctionWindow::saveFile(char* fileName){
+    fprintf(fp, "%d\n", tf_numOfPoints);
 
-	// Check for bad param
-	if(fileName == NULL)
-		return;
-	if(strlen(fileName) < 1)
-		return;
+    ControlPoint* cur=tf_pointList;
 
-	FILE* fp = fopen(fileName, "w");
+    for (int i=0; i<tf_numOfPoints; i++) {
+        fprintf(fp, "%g %g\n", ((double)(cur->x-15))/((double)tf_unitWidth), ((double)(cur->y-15))/((double)tf_winy-15));        
+        cur=cur->next;
+    }
+    fprintf(fp, "\n");
 
-	if(!fp)
-		return; //error
+    fprintf(fp, "%d\n", map->numOfKeys);
+    cur=map->keyList;
+    for (int i=0; i<map->numOfKeys; i++) {
+        fprintf(fp, "%g\n", ((double)(cur->x-15))/((double)cm_unitWidth));
+        cur=cur->next;
+    }
+    fprintf(fp, "\n");
 
-	fprintf(fp, "%d\n", tf_numOfPoints);
+    Color* curColor=map->keyColors;
+    for (int i=0; i<map->numOfKeys; i++) {
+        fprintf(fp, "%g %g %g\n", curColor->R, curColor->G, curColor->B);
+        curColor=curColor->next;
+    }
+    fprintf(fp, "\n");
 
-	ControlPoint* cur=tf_pointList;
+    //eye location
+    //fprintf(fp, "%g %g %g\n\n", eye_shift_x, eye_shift_y, eyedistance);
 
-	for (int i=0; i<tf_numOfPoints; i++){
-		fprintf(fp, "%g %g\n", ((double)(cur->x-15))/((double)tf_unitWidth), ((double)(cur->y-15))/((double)tf_winy-15));	
-		cur=cur->next;
-	}
-	fprintf(fp, "\n");
+    //object center
+    //fprintf(fp, "%g %g %g\n\n", obj_center[0], obj_center[1], obj_center[2]);
 
+    //bluntfin quater
+    //fprintf(fp, "%g %g %g %g\n\n", bluntfin_quater.s, bluntfin_quater.x, bluntfin_quater.y, bluntfin_quater.z);
 
-	fprintf(fp, "%d\n", map->numOfKeys);
-	cur=map->keyList;
-	for(int i=0; i<map->numOfKeys; i++){
-		fprintf(fp, "%g\n", ((double)(cur->x-15))/((double)cm_unitWidth));
-		cur=cur->next;
-	}
-	fprintf(fp, "\n");
+    //scale and field_of_view
+    //fprintf(fp, "%g %g\n", control_point_scale, fovy);
 
-
-	Color* curColor=map->keyColors;
-	for(int i=0; i<map->numOfKeys; i++){
-		fprintf(fp, "%g %g %g\n", curColor->R, curColor->G, curColor->B);
-		curColor=curColor->next;
-	}
-	fprintf(fp, "\n");
-
-	//eye location
-	//fprintf(fp, "%g %g %g\n\n", eye_shift_x, eye_shift_y, eyedistance);
-
-	//object center
-	//fprintf(fp, "%g %g %g\n\n", obj_center[0], obj_center[1], obj_center[2]);
-
-	//bluntfin quater
-	//fprintf(fp, "%g %g %g %g\n\n", bluntfin_quater.s, bluntfin_quater.x, bluntfin_quater.y, bluntfin_quater.z);
-
-	//scale and field_of_view
-	//fprintf(fp, "%g %g\n", control_point_scale, fovy);
-
-	fprintf(fp, "END\n");
-	
-	fclose(fp);
+    fprintf(fp, "END\n");
+        
+    fclose(fp);
 }
