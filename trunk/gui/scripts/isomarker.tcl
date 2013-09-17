acd3a77d6ff4e78fd42fde3263ca0575b038f5fb
gah <gah@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2013-09-17 16:32:26 +0000

    fix limits in nanovis to display title/units

--- trunk/gui/scripts/isomarker.tcl
+++ trunk/gui/scripts/isomarker.tcl
@@ -29,136 +29,158 @@ itcl::class Rappture::IsoMarker {
     private variable _activePress    0
     private common   _normalIcon [Rappture::icon nvlegendmark]
     private common   _activeIcon [Rappture::icon nvlegendmark2]
+    private method EnterTick {} 
+    private method LeaveTick {} 
+    private method StartDrag { x y } 
+    private method ContinueDrag { x y } 
+    private method StopDrag { x y } 
 
-    constructor {c obj tf args} { 
-        set _canvas $c
-        set _nvobj $obj
-        set _tf $tf
-        set w [winfo width $_canvas]
-        set h [winfo height $_canvas]
-        set _tick [$c create image 0 $h \
-                -image $_normalIcon -anchor s \
-                -tags "$this $obj" -state hidden]
-        set _label [$c create text 0 $h \
-                -anchor n -fill white -font "Helvetica 8" \
-                -tags "$this $obj" -state hidden]
-        $c bind $_tick <Enter> [itcl::code $this HandleEvent "enter"]
-        $c bind $_tick <Leave> [itcl::code $this HandleEvent "leave"]
-        $c bind $_tick <ButtonPress-1> \
-            [itcl::code $this HandleEvent "start" %x %y]
-        $c bind $_tick <B1-Motion> \
-            [itcl::code $this HandleEvent "update" %x %y]
-        $c bind $_tick <ButtonRelease-1> \
-            [itcl::code $this HandleEvent "end" %x %y]
-    }
-    destructor { 
-        $_canvas delete $this
-    }
-    public method transferfunc {} {
-        return $_tf
-    }
-    public method activate { bool } {
-        if  { $bool || $_activePress || $_activeMotion } {
-            $_canvas itemconfigure $_label -state normal
-            $_canvas itemconfigure $_tick -image $_activeIcon
-        } else {
-            $_canvas itemconfigure $_label -state hidden
-            $_canvas itemconfigure $_tick -image $_normalIcon
-        }
-    }
-    public method visible { bool } {
-        if { $bool } {
-            absval $_value
-            $_canvas itemconfigure $_tick -state normal
-            $_canvas raise $_tick
-        } else {
-            $_canvas itemconfigure $_tick -state hidden
-        }
-    }
-    public method screenpos { } { 
-        set x [relval]
-        if { $x < 0.0 } {
-            set x 0.0
-        } elseif { $x > 1.0 } {
-            set x 1.0 
-        }
-        set low 10 
-        set w [winfo width $_canvas]
-        set high [expr {$w  - 10}]
-        set x [expr {round($x*($high - $low) + $low)}]
-        return $x
-    }
-    public method absval { {x "-get"} } {
-        if { $x != "-get" } {
-            set _value $x
-            set y 31
-            $_canvas itemconfigure $_label -text [format %.2g $_value]
-            set x [screenpos]
-            $_canvas coords $_tick $x [expr {$y+3}]
-            $_canvas coords $_label $x [expr {$y+5}]
-        }
-        return $_value
-    }
-    public method relval  { {x "-get"} } {
-        if { $x == "-get" } {
-            array set limits [$_nvobj limits $_tf] 
-            if { $limits(vmax) == $limits(vmin) } {
-                if { $limits(vmax) == 0.0 } {
-                    set limits(vmin) 0.0
-                    set limits(vmax) 1.0
-                } else {
-                    set limits(vmax) [expr $limits(vmin) + 1.0]
-                }
-            }
-            return [expr {($_value-$limits(vmin))/
-                          ($limits(vmax) - $limits(vmin))}]
-        } 
-        array set limits [$_nvobj limits $_tf] 
-        if { $limits(vmax) == $limits(vmin) } {
-            set limits(vmin) 0.0
-            set limits(vmax) 1.0
-        }
-        if { [catch {expr $limits(vmax) - $limits(vmin)} r] != 0 } {
-            return 0.0
-        }           
-        absval [expr {($x * $r) + $limits(vmin)}]
-    }
-    private method HandleEvent { option args } {
-        switch -- $option {
-            enter {
-                set _activeMotion 1
-                activate yes
-                $_canvas raise $_tick
-            }
-            leave {
-                set _activeMotion 0
-                activate no
-            }
-            start {
-                $_canvas raise $_tick 
-                set _activePress 1
-                activate yes
-                $_canvas itemconfigure limits -state hidden
-            }
-            update {
-                set w [winfo width $_canvas]
-                set x [lindex $args 0]
-                relval [expr {double($x-10)/($w-20)}]
-                $_nvobj overmarker $this $x
-                $_nvobj updatetransferfuncs
-            }
-            end {
-                set x [lindex $args 0]
-                if { ![$_nvobj rmdupmarker $this $x]} {
-                    eval HandleEvent update $args
-                }
-                set _activePress 0
-                activate no
-                $_canvas itemconfigure limits -state normal
-            }
-            default {
-                error "bad option \"$option\": should be start, update, end"
-            }
-        }
+    constructor {c obj tf args} {}
+    destructor {} 
+    public method transferfunc {} 
+    public method activate { bool } 
+    public method visible { bool } 
+    public method screenpos {} 
+    public method absval { {x "-get"} } 
+    public method relval  { {x "-get"} }
+}
+
+itcl::body Rappture::IsoMarker::constructor {c obj tf args} { 
+    set _canvas $c
+    set _nvobj $obj
+    set _tf $tf
+    set w [winfo width $_canvas]
+    set h [winfo height $_canvas]
+    set _tick [$c create image 0 $h \
+                   -image $_normalIcon -anchor s \
+                   -tags "$this $obj" -state hidden]
+    set _label [$c create text 0 $h \
+                    -anchor n -fill white -font "Helvetica 8" \
+                    -tags "$this $obj" -state hidden]
+    $c bind $_tick <Enter>           [itcl::code $this EnterTick]
+    $c bind $_tick <Leave>           [itcl::code $this LeaveTick]
+    $c bind $_tick <ButtonPress-1>   [itcl::code $this StartDrag %x %y]
+    $c bind $_tick <B1-Motion>       [itcl::code $this ContinueDrag %x %y]
+    $c bind $_tick <ButtonRelease-1> [itcl::code $this StopDrag %x %y]
+}
+
+itcl::body Rappture::IsoMarker::destructor {} {
+    $_canvas delete $this
+}
+
+itcl::body Rappture::IsoMarker::transferfunc {} {
+    return $_tf
+}
+
+itcl::body Rappture::IsoMarker::activate { bool } {
+    if  { $bool || $_activePress || $_activeMotion } {
+        $_canvas itemconfigure $_label -state normal
+        $_canvas itemconfigure $_tick -image $_activeIcon
+        $_canvas itemconfigure title -state hidden
+    } else {
+        $_canvas itemconfigure $_label -state hidden
+        $_canvas itemconfigure $_tick -image $_normalIcon
+        $_canvas itemconfigure title -state normal
     }
 }
+
+itcl::body Rappture::IsoMarker::visible { bool } {
+    if { $bool } {
+        absval $_value
+        $_canvas itemconfigure $_tick -state normal
+        $_canvas raise $_tick
+    } else {
+        $_canvas itemconfigure $_tick -state hidden
+    }
+}
+
+itcl::body Rappture::IsoMarker::screenpos { } { 
+    set x [relval]
+    if { $x < 0.0 } {
+        set x 0.0
+    } elseif { $x > 1.0 } {
+        set x 1.0 
+    }
+    set low 10 
+    set w [winfo width $_canvas]
+    set high [expr {$w  - 10}]
+    set x [expr {round($x*($high - $low) + $low)}]
+    return $x
+}
+
+itcl::body Rappture::IsoMarker::absval { {x "-get"} } {
+    if { $x != "-get" } {
+        set _value $x
+        set y 31
+        $_canvas itemconfigure $_label -text [format %.2g $_value]
+        set x [screenpos]
+        $_canvas coords $_tick $x [expr {$y+3}]
+        $_canvas coords $_label $x [expr {$y+5}]
+    }
+    return $_value
+}
+
+itcl::body Rappture::IsoMarker::relval  { {x "-get"} } {
+    foreach {min max} [$_nvobj limits $_tf] break
+    if { $x == "-get" } {
+        if { $max == $min } {
+            if { $max == 0.0 } {
+                set min 0.0
+                set max 1.0
+            } else {
+                set max [expr $min + 1.0]
+            }
+        }
+        return [expr {($_value - $min) / ($max - $min)}]
+    } 
+    if { $max == $min } {
+        set min 0.0
+        set max 1.0
+    }
+    if { [catch {expr $max - $min} r] != 0 } {
+        return 0.0
+    }           
+    absval [expr {($x * $r) + $min}]
+}
+
+itcl::body Rappture::IsoMarker::EnterTick {} {
+    set _activeMotion 1
+    activate yes
+    $_canvas raise $_tick
+}
+
+itcl::body Rappture::IsoMarker::LeaveTick {} {
+    set _activeMotion 0
+    activate no
+}
+
+itcl::body Rappture::IsoMarker::StartDrag { x y } {
+    $_canvas raise $_tick 
+    set _activePress 1
+    activate yes
+    $_canvas itemconfigure limits -state hidden
+    $_canvas itemconfigure title -state hidden
+}
+
+itcl::body Rappture::IsoMarker::StopDrag { x y } {
+    if { ![$_nvobj removeDuplicateMarker $this $x]} {
+        ContinueDrag $x $y 
+    }
+    set _activePress 0
+    activate no
+    $_canvas itemconfigure limits -state normal
+    $_canvas itemconfigure title -state normal
+}
+
+itcl::body Rappture::IsoMarker::ContinueDrag { x y } {
+    set w [winfo width $_canvas]
+    relval [expr {double($x-10)/($w-20)}]
+    $_nvobj overMarker $this $x
+    $_nvobj updateTransferFunctions
+    $_canvas raise $_tick 
+    set _activePress 1
+    activate yes
+    $_canvas itemconfigure limits -state hidden
+    $_canvas itemconfigure title -state hidden
+}
+
