b252d1cf0c7eb7164ec4de09c3cc92c554241fae
ldelgass <ldelgass@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2013-10-27 07:18:57 +0000

    Add mouse events

--- packages/vizservers/geovis/Renderer.h
+++ packages/vizservers/geovis/Renderer.h
@@ -24,7 +24,9 @@
 #include <osgEarth/ModelLayer>
 #include <osgEarth/TileSource>
 #include <osgEarth/ModelSource>
+#include <osgEarth/GeoData>
 #include <osgEarthUtil/EarthManipulator>
+#include <osgEarthUtil/MouseCoordsTool>
 
 #include "Types.h"
 #include "Trace.h"
@@ -75,6 +77,44 @@ public:
 
 private:
     osg::ref_ptr<osg::Image> _image;
+};
+
+class MouseCoordsCallback : public osgEarth::Util::MouseCoordsTool::Callback
+{
+public:
+    MouseCoordsCallback() :
+        osgEarth::Util::MouseCoordsTool::Callback(),
+        _havePoint(false)
+    {}
+
+    void set(const osgEarth::GeoPoint& p, osg::View* view, osgEarth::MapNode* mapNode)
+    {
+        // p.y(), p.x()
+        _pt = p;
+        _havePoint = true;
+    }
+
+    void reset(osg::View* view, osgEarth::MapNode* mapNode)
+    {
+        // Out of range click
+        //_havePoint = false;
+    }
+
+    bool report(double *x, double *y, double *z)
+    {
+        if (_havePoint) {
+            *x = _pt.x();
+            *y = _pt.y();
+            *z = _pt.z();
+            _havePoint = false;
+            return true;
+        }
+        return false;
+    }
+
+private:
+    bool _havePoint;
+    osgEarth::GeoPoint _pt;
 };
 
 /**
@@ -142,6 +182,20 @@ public:
 
     void zoomCamera(double z, bool absolute = false);
 
+    // Mouse events
+
+    void mouseClick(int button, double x, double y);
+
+    void mouseDoubleClick(int button, double x, double y);
+
+    void mouseDrag(int button, double x, double y);
+
+    void mouseRelease(int button, double x, double y);
+
+    void mouseMotion(double x, double y);
+
+    void mouseScroll(int direction);
+
     // Rendering an image
 
     void setBackgroundColor(float color[3]);
@@ -152,6 +206,11 @@ public:
 
     osg::Image *getRenderedFrame();
 
+    bool getMousePoint(double *x, double *y, double *z)
+    {
+        return _coordsCallback->report(x, y, z);
+    }
+
 private:
     void initCamera();
 
@@ -160,9 +219,12 @@ private:
     float _bgColor[3];
 
     osg::ref_ptr<osg::Node> _sceneRoot;
+    osg::ref_ptr<osgEarth::MapNode> _mapNode;
     osg::ref_ptr<osgEarth::Map> _map;
     osg::ref_ptr<osgViewer::Viewer> _viewer;
     osg::ref_ptr<ScreenCaptureCallback> _captureCallback;
+    osg::ref_ptr<osgEarth::Util::MouseCoordsTool> _mouseCoordsTool;
+    osg::ref_ptr<MouseCoordsCallback> _coordsCallback;
     osg::ref_ptr<osgEarth::Util::EarthManipulator> _manipulator;
 };
 
