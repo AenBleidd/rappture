cd637096fe492cc91ce697c9d391a1a700c559ca
ldelgass <ldelgass@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2014-08-14 20:16:03 +0000

    Remove vizservers from trunk

--- packages/vizservers/aclocal.m4
+++
@@ -1,2 +0,0 @@
-builtin(include,./cf/tcl.m4)
-builtin(include,./cf/rpLangPython.m4)
--- packages/vizservers/nanovis/vrmath/Vector2f.cpp
+++
@@ -1,8 +0,0 @@
-/* -*- mode: c++; c-basic-offset: 4; indent-tabs-mode: nil -*- */
-/*
- * Copyright (c) 2004-2013  HUBzero Foundation, LLC
- *
- * Author: Insoo Woo <iwoo@purdue.edu>
- */
-
-#include <vrmath/Vector2f.h>
