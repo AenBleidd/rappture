49dd8907c204626f623b02008b4856b107039208
ldelgass <ldelgass@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2014-11-25 21:03:49 +0000

    Add tool hints to drawing output

--- trunk/gui/scripts/drawing.tcl
+++ trunk/gui/scripts/drawing.tcl
@@ -128,6 +128,18 @@ itcl::body Rappture::Drawing::constructor {xmlobj path} {
             set _hints($key) $str
         }
     }
+    foreach {key path} {
+        toolid          tool.id
+        toolname        tool.name
+        toolcommand     tool.execute
+        tooltitle       tool.title
+        toolrevision    tool.version.application.revision
+    } {
+        set str [$_xmlobj get $path]
+        if { "" != $str } {
+            set _hints($key) $str
+        }
+    }
 }
 
 # ----------------------------------------------------------------------
