838e378c3f8ccb7783458a96f0a0e4b940e1b738
ldelgass <ldelgass@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2015-01-21 09:44:08 +0000

    add Rappture build info to map clientinfo

--- trunk/gui/scripts/mapviewer.tcl
+++ trunk/gui/scripts/mapviewer.tcl
@@ -860,6 +860,10 @@ itcl::body Rappture::MapViewer::Connect {} {
             if { [info exists env(SESSION)] } {
                 set session $env(SESSION)
             }
+            lappend info "version" "$Rappture::version"
+            lappend info "build" "$Rappture::build"
+            lappend info "svnurl" "$Rappture::svnurl"
+            lappend info "installdir" "$Rappture::installdir"
             lappend info "hub" [exec hostname]
             lappend info "client" "mapviewer"
             lappend info "user" $user
