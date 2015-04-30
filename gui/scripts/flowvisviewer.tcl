5daac7ae1b6e30fcb3af0ad37efb12e4c6a1e093
ldelgass <ldelgass@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2015-04-30 16:00:56 +0000

    whitespace/comment fixes

--- gui/scripts/flowvisviewer.tcl
+++ gui/scripts/flowvisviewer.tcl
@@ -3,14 +3,14 @@
 #  COMPONENT: flowvisviewer - 3D flow rendering
 #
 #  This widget performs volume and flow rendering on 3D scalar/vector datasets.
-#  It connects to the Flowvis server running on a rendering farm, transmits
-#  data, and displays the results.
+#  It connects to the Nanovis server running on a rendering farm,
+#  transmits data, and displays the results.
 # ======================================================================
 #  AUTHOR:  Michael McLennan, Purdue University
 #  Copyright (c) 2004-2012  HUBzero Foundation, LLC
 #
-#  See the file "license.terms" for information on usage and redistribution of
-#  this file, and for a DISCLAIMER OF ALL WARRANTIES.
+#  See the file "license.terms" for information on usage and
+#  redistribution of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 # ======================================================================
 package require Itk
 package require BLT
