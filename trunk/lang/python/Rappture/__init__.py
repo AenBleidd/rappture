366ec82bc9d98a418bbd0e88cf09df10edd65c09
ldelgass <ldelgass@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2015-05-21 11:28:27 +0000

    Merge r5557:5561,r5574:5575 from 1.3 branch

--- trunk/lang/python/Rappture/__init__.py
+++ trunk/lang/python/Rappture/__init__.py
@@ -1,5 +1,6 @@
 # Rappture package
 from library import library
+from pyxml import PyXml
 from interface import interface
 from number import number
 from result import result
@@ -8,3 +9,9 @@ import tools as tools
 import Units
 import Utils
 import encoding
+
+# encoding/decoding flags
+RPENC_Z   = 1
+RPENC_B64 = 2
+RPENC_ZB64 = 3
+RPENC_RAW = 8
