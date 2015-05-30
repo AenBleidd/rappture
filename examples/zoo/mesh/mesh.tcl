88c65246abd169234446f06b724cd715d127ccb2
ldelgass <ldelgass@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2015-05-30 02:22:49 +0000

    add structured grid example/test

--- examples/zoo/mesh/mesh.tcl
+++ examples/zoo/mesh/mesh.tcl
@@ -20,162 +20,181 @@ if { $contour  == "yes" } {
 set xv [blt::vector create \#auto]
 $xv seq 0 1 50
 
+set hide no
+
 switch -- $meshtype {
     "cloud" {
-	set mesh output.mesh
+        set mesh output.mesh
 
-	$driver put $mesh.about.label "cloud in unstructured mesh"
-	$driver put $mesh.dim  2
-	$driver put $mesh.units "m"
-	$driver put $mesh.hide "yes"
+        $driver put $mesh.about.label "cloud in unstructured mesh"
+        $driver put $mesh.dim  2
+        $driver put $mesh.units "m"
+        $driver put $mesh.hide $hide
 
-	set points {}
-	foreach y [$xv range 0 end] {
-	    foreach x [$xv range 0 end] {
-		append points "$x $y\n"
-	    }
-	}
-	$driver put $mesh.unstructured.points $points
-	$driver put $mesh.unstructured.celltypes ""
+        set points {}
+        foreach y [$xv range 0 end] {
+            foreach x [$xv range 0 end] {
+                append points "$x $y\n"
+            }
+        }
+        $driver put $mesh.unstructured.points $points
+        $driver put $mesh.unstructured.celltypes ""
     }
     "regular" {
-	set mesh output.mesh
+        set mesh output.mesh
 
-	$driver put $mesh.about.label "uniform grid mesh"
-	$driver put $mesh.dim  2
-	$driver put $mesh.units "m"
-	$driver put $mesh.hide "yes"
+        $driver put $mesh.about.label "uniform grid mesh"
+        $driver put $mesh.dim  2
+        $driver put $mesh.units "m"
+        $driver put $mesh.hide $hide
 
-	$driver put $mesh.grid.xaxis.min 0.0 
-	$driver put $mesh.grid.xaxis.max 1.0
-	$driver put $mesh.grid.xaxis.numpoints 50
-	$driver put $mesh.grid.yaxis.min 0.0 
-	$driver put $mesh.grid.yaxis.max 1.0
-	$driver put $mesh.grid.yaxis.numpoints 50
+        $driver put $mesh.grid.xaxis.min 0.0 
+        $driver put $mesh.grid.xaxis.max 1.0
+        $driver put $mesh.grid.xaxis.numpoints 50
+        $driver put $mesh.grid.yaxis.min 0.0 
+        $driver put $mesh.grid.yaxis.max 1.0
+        $driver put $mesh.grid.yaxis.numpoints 50
     }
     "irregular" {
-	set mesh output.mesh
+        set mesh output.mesh
 
-	$driver put $mesh.about.label "irregular grid mesh"
-	$driver put $mesh.dim  2
-	$driver put $mesh.units "m"
-	$driver put $mesh.hide "yes"
+        $driver put $mesh.about.label "irregular rectilinear grid mesh"
+        $driver put $mesh.dim  2
+        $driver put $mesh.units "m"
+        $driver put $mesh.hide $hide
 
-	$driver put $mesh.grid.xcoords [$xv range 0 end]
-	$driver put $mesh.grid.ycoords [$xv range 0 end]
+        $driver put $mesh.grid.xcoords [$xv range 0 end]
+        $driver put $mesh.grid.ycoords [$xv range 0 end]
     }
     "hybrid" {
-	set mesh output.mesh
+        set mesh output.mesh
 
-	$driver put $mesh.about.label "hybrid regular and irregular grid mesh"
-	$driver put $mesh.dim  2
-	$driver put $mesh.units "m"
-	$driver put $mesh.hide "yes"
+        $driver put $mesh.about.label "hybrid regular and irregular rectilinear grid mesh"
+        $driver put $mesh.dim  2
+        $driver put $mesh.units "m"
+        $driver put $mesh.hide $hide
 
-	$driver put $mesh.units "m"
-	$driver put $mesh.grid.xcoords [$xv range 0 end]
-	$driver put $mesh.grid.yaxis.min 0.0 
-	$driver put $mesh.grid.yaxis.max 1.0
-	$driver put $mesh.grid.yaxis.numpoints 50
+        $driver put $mesh.grid.xcoords [$xv range 0 end]
+        $driver put $mesh.grid.yaxis.min 0.0 
+        $driver put $mesh.grid.yaxis.max 1.0
+        $driver put $mesh.grid.yaxis.numpoints 50
+    }
+    "structured" {
+        set mesh output.mesh
+
+        $driver put $mesh.about.label "Structured (Curvilinear) Grid"
+        $driver put $mesh.dim  2
+        $driver put $mesh.units "m"
+        $driver put $mesh.hide $hide
+
+        $driver put $mesh.grid.xdim 50
+        $driver put $mesh.grid.ydim 50
+        set points {}
+        foreach y [$xv range 0 end] {
+            foreach x [$xv range 0 end] {
+                append points "$x $y\n"
+            }
+        }
+        $driver put $mesh.grid.points $points
     }
     "triangular" {
-	set mesh output.mesh
+        set mesh output.mesh
 
-	$driver put $mesh.about.label "triangles in unstructured mesh"
-	$driver put $mesh.dim  2
-	$driver put $mesh.units "m"
-	$driver put $mesh.hide "yes"
-	
-	$driver put -type file -compress no $mesh.unstructured.points \
-	    points.txt
-	$driver put -type file -compress no $mesh.unstructured.triangles \
-	    triangles.txt
+        $driver put $mesh.about.label "triangles in unstructured mesh"
+        $driver put $mesh.dim  2
+        $driver put $mesh.units "m"
+        $driver put $mesh.hide $hide
+        
+        $driver put -type file -compress no $mesh.unstructured.points \
+            points.txt
+        $driver put -type file -compress no $mesh.unstructured.triangles \
+            triangles.txt
     }
     "generic" {
-	set mesh output.mesh
+        set mesh output.mesh
 
-	$driver put $mesh.about.label "nodes and elements mesh"
-	$driver put $mesh.dim  2
-	$driver put $mesh.units "m"
-	$driver put $mesh.hide "yes"
+        $driver put $mesh.about.label "nodes and elements mesh"
+        $driver put $mesh.dim  2
+        $driver put $mesh.units "m"
+        $driver put $mesh.hide $hide
 
-	set count 0
-	set f [open "points.txt" "r"]
-	set points [read $f]
-	close $f
-	foreach { x y } $points {
-	    $driver put $mesh.node($count) "$x $y"
-	    incr count
-	}
-	set count 0
-	set f [open "triangles.txt" "r"]
-	set triangles [read $f]
-	close $f
-	foreach { a b c } $triangles {
-	    $driver put $mesh.element($count).nodes "$a $b $c"
-	    incr count
-	}
+        set count 0
+        set f [open "points.txt" "r"]
+        set points [read $f]
+        close $f
+        foreach { x y } $points {
+            $driver put $mesh.node($count) "$x $y"
+            incr count
+        }
+        set count 0
+        set f [open "triangles.txt" "r"]
+        set triangles [read $f]
+        close $f
+        foreach { a b c } $triangles {
+            $driver put $mesh.element($count).nodes "$a $b $c"
+            incr count
+        }
     }
     "unstructured" {
-	set mesh output.mesh
+        set mesh output.mesh
 
-	$driver put $mesh.about.label "Unstructured Grid"
-	$driver put $mesh.dim  2
-	$driver put $mesh.units "m"
-	$driver put $mesh.hide "yes"
+        $driver put $mesh.about.label "Unstructured Grid"
+        $driver put $mesh.dim  2
+        $driver put $mesh.units "m"
+        $driver put $mesh.hide $hide
 
-	$driver put -type file -compress no $mesh.unstructured.points points.txt
-	set cells {}
-	set f [open "triangles.txt" "r"]
-	set triangles [read $f]
-	close $f
-	foreach { a b c } $triangles {
-	    append cells "$a $b $c\n"
-	}
-	$driver put $mesh.unstructured.cells $cells
-	$driver put $mesh.unstructured.celltypes "triangle"
+        $driver put -type file -compress no $mesh.unstructured.points points.txt
+        set cells {}
+        set f [open "triangles.txt" "r"]
+        set triangles [read $f]
+        close $f
+        foreach { a b c } $triangles {
+            append cells "$a $b $c\n"
+        }
+        $driver put $mesh.unstructured.cells $cells
+        $driver put $mesh.unstructured.celltypes "triangle"
     }
     "cells" {
-	set mesh output.mesh
+        set mesh output.mesh
 
-	$driver put $mesh.about.label \
-	    "Unstructured Grid with Heterogeneous Cells"
-	$driver put $mesh.dim  2
-	$driver put $mesh.units "m"
-	$driver put $mesh.hide "yes"
+        $driver put $mesh.about.label \
+            "Unstructured Grid with Heterogeneous Cells"
+        $driver put $mesh.dim  2
+        $driver put $mesh.units "m"
+        $driver put $mesh.hide $hide
 
-	set celltypes {}
-	set f [open "triangles.txt" "r"]
-	set triangles [read $f]
-	close $f
-	foreach { a b c } $triangles {
-	    append cells "$a $b $c\n"
-	    append celltypes "triangle\n"
-	}
-	$driver put -type file -compress no $mesh.unstructured.points points.txt
-	$driver put $mesh.unstructured.celltypes $celltypes
-	$driver put $mesh.unstructured.cells $cells
+        set celltypes {}
+        set f [open "triangles.txt" "r"]
+        set triangles [read $f]
+        close $f
+        foreach { a b c } $triangles {
+            append cells "$a $b $c\n"
+            append celltypes "triangle\n"
+        }
+        $driver put -type file -compress no $mesh.unstructured.points points.txt
+        $driver put $mesh.unstructured.celltypes $celltypes
+        $driver put $mesh.unstructured.cells $cells
     }
     "vtkmesh" {
-	set mesh output.mesh
+        set mesh output.mesh
 
-	$driver put $mesh.about.label "vtk mesh"
-	$driver put $mesh.dim  2
-	$driver put $mesh.units "m"
-	$driver put $mesh.hide "yes"
-	$driver put -type file -compress no $mesh.vtk mesh.vtk
+        $driver put $mesh.about.label "vtk mesh"
+        $driver put $mesh.dim  2
+        $driver put $mesh.units "m"
+        $driver put $mesh.hide $hide
+        $driver put -type file -compress no $mesh.vtk mesh.vtk
     }
     "vtkfield" {
 
-	$driver put output.field(substrate).about.label "Substrate Surface"
-	$driver put -type file -compress no \
-	    output.field(substrate).component.vtk file.vtk
+        $driver put output.field(substrate).about.label "Substrate Surface"
+        $driver put -type file -compress no \
+            output.field(substrate).component.vtk file.vtk
         $driver put output.string.current ""
-	Rappture::result $driver
-	exit 0
+        Rappture::result $driver
+        exit 0
     }
     default {
-	error "unknown mesh type \"$meshtype\""
+        error "unknown mesh type \"$meshtype\""
     }
 }
 
