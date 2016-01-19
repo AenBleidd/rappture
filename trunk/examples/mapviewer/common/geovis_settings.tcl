60d580638c3c33cf6ba75699a4ed31fbdd325f72
ldelgass <ldelgass@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2016-01-19 19:13:31 +0000

    revert r5956

--- trunk/examples/mapviewer/common/geovis_settings.tcl
+++
@@ -1,7 +0,0 @@
-
-set Rappture::version "1.5"
-set Rappture::svnurl "http://rappture.org"
-set Rappture::installdir "/apps/share64"
-set Rappture::build "5934M"
-set GEOVIS_SERVERS [list render09.nanohub.org:2015]
-
