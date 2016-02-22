31d1f7d10be031196687a7e51c57ec1f211cd11c
ldelgass <ldelgass@0ab1a430-77f2-0310-bf05-c480b4e6bd5a>
2016-02-22 20:20:01 +0000

    Merge r5990:6034 from 1.4 branch (updates to tools.py)

--- trunk/lang/python/Rappture/tools.py
+++ trunk/lang/python/Rappture/tools.py
@@ -14,6 +14,7 @@ import shlex
 import select
 import signal
 import traceback
+import time
 
 commandPid = 0
 
@@ -23,9 +24,15 @@ def sig_handler(signalType, frame):
         os.kill(commandPid,signal.SIGTERM)
 
 
-def getCommandOutput(command,
-                     streamOutput=False):
+def executeCommand(command,
+                   stdin=None,
+                   streamOutput=False):
     global commandPid
+
+    exitStatus = 0
+    fpStdin = None
+    outData = []
+    errData = []
 
     try:
        sig_INT_handler = signal.signal(signal.SIGINT,sig_handler)
@@ -37,91 +44,146 @@ def getCommandOutput(command,
     except:
        print traceback.format_exc()
 
-    BUFSIZ = 4096
-    if isinstance(command,list):
-       child = subprocess.Popen(command,bufsize=BUFSIZ, \
-                                stdout=subprocess.PIPE, \
-                                stderr=subprocess.PIPE, \
-                                close_fds=True)
+    if stdin == None:
+       commandStdin = None
     else:
-       try:
-          commandStr = str(command)
-       except UnicodeEncodeError:
-          commandStr = unicode(command).encode('unicode_escape')
-       commandArgs = shlex.split(commandStr)
-       child = subprocess.Popen(commandArgs,bufsize=BUFSIZ, \
-                                stdout=subprocess.PIPE, \
-                                stderr=subprocess.PIPE, \
-                                close_fds=True)
-    commandPid = child.pid
-    childout   = child.stdout
-    childoutFd = childout.fileno()
-    childerr   = child.stderr
-    childerrFd = childerr.fileno()
-
-    outEOF = False
-    errEOF = False
-
-    outData = []
-    errData = []
-
-    while True:
-        toCheck = []
-        if not outEOF:
-            toCheck.append(childoutFd)
-        if not errEOF:
-            toCheck.append(childerrFd)
-        try:
-            readyRead,readyWrite,readyException = select.select(toCheck,[],[]) # wait for input
-        except select.error,err:
-            readyRead = []
-            readyWrite = []
-            readyException = []
-        if childoutFd in readyRead:
-            outChunk = os.read(childoutFd,BUFSIZ)
-            if outChunk == '':
-                outEOF = True
-            outData.append(outChunk)
-            if streamOutput:
-                sys.stdout.write(outChunk)
-                sys.stdout.flush()
-
-        if childerrFd in readyRead:
-            errChunk = os.read(childerrFd,BUFSIZ)
-            if errChunk == '':
-                errEOF = True
-            errData.append(errChunk)
-            if streamOutput:
-                sys.stderr.write(errChunk)
+       if   isinstance(stdin,file):
+          commandStdin = stdin
+       elif isinstance(stdin,int):
+          commandStdin = stdin
+       else:
+          try:
+             stdinPath = str(stdin)
+          except UnicodeEncodeError:
+             try:
+                stdinPath = unicode(stdin).encode('unicode_escape')
+             except:
+                stdinPath = None
+          except:
+             stdinPath = None
+          if stdinPath:
+             if os.path.isfile(stdinPath):
+                try:
+                   fpStdin = open(stdinPath,'r')
+                except (IOError,OSError):
+                   exitStatus = 1
+                   errData.append("File %s could not be opened.\n" % (stdinPath))
+                   if streamOutput:
+                      sys.stderr.write("File %s could not be opened.\n" % (stdinPath))
+                      sys.stderr.flush()
+                else:
+                   commandStdin = fpStdin
+             else:
+                exitStatus = 1
+                errData.append("File %s does not exist.\n" % (stdinPath))
+                if streamOutput:
+                   sys.stderr.write("File %s does not exist.\n" % (stdinPath))
+                   sys.stderr.flush()
+          else:
+             exitStatus = 1
+             errData.append("Bad argument type specified for stdin.\n")
+             if streamOutput:
+                sys.stderr.write("Bad argument type specified for stdin.\n")
                 sys.stderr.flush()
 
-        if outEOF and errEOF:
-            break
+    if not exitStatus:
+       BUFSIZ = 4096
+       if isinstance(command,list):
+          child = subprocess.Popen(command,bufsize=BUFSIZ, \
+                                   stdin=commandStdin, \
+                                   stdout=subprocess.PIPE, \
+                                   stderr=subprocess.PIPE, \
+                                   close_fds=True)
+       else:
+          try:
+             commandStr = str(command)
+          except UnicodeEncodeError:
+             commandStr = unicode(command).encode('unicode_escape')
+          commandArgs = shlex.split(commandStr)
+          child = subprocess.Popen(commandArgs,bufsize=BUFSIZ, \
+                                   stdin=commandStdin, \
+                                   stdout=subprocess.PIPE, \
+                                   stderr=subprocess.PIPE, \
+                                   close_fds=True)
+       commandPid = child.pid
+       childout   = child.stdout
+       childoutFd = childout.fileno()
+       childerr   = child.stderr
+       childerrFd = childerr.fileno()
+ 
+       outEOF = False
+       errEOF = False
+ 
+       while True:
+           toCheck = []
+           if not outEOF:
+               toCheck.append(childoutFd)
+           if not errEOF:
+               toCheck.append(childerrFd)
+           try:
+               readyRead,readyWrite,readyException = select.select(toCheck,[],[]) # wait for input
+           except select.error,err:
+               readyRead = []
+               readyWrite = []
+               readyException = []
+           if childoutFd in readyRead:
+               outChunk = os.read(childoutFd,BUFSIZ)
+               if outChunk == '':
+                   outEOF = True
+               outData.append(outChunk)
+               if streamOutput:
+                   sys.stdout.write(outChunk)
+                   sys.stdout.flush()
+ 
+           if childerrFd in readyRead:
+               errChunk = os.read(childerrFd,BUFSIZ)
+               if errChunk == '':
+                   errEOF = True
+               errData.append(errChunk)
+               if streamOutput:
+                   sys.stderr.write(errChunk)
+                   sys.stderr.flush()
+ 
+           if outEOF and errEOF:
+               break
+ 
+       pid,exitStatus = os.waitpid(commandPid,0)
+       commandPid = 0
+       if fpStdin:
+          try:
+             fpStdin.close()
+          except:
+             pass
+ 
+       try:
+          signal.signal(signal.SIGINT,sig_INT_handler)
+          signal.signal(signal.SIGHUP,sig_HUP_handler)
+          signal.signal(signal.SIGTERM,sig_TERM_handler)
+       except UnboundLocalError:
+   #      happens when used in a thread
+          pass
+       except:
+          print traceback.format_exc()
+ 
+       if exitStatus != 0:
+           if   os.WIFSIGNALED(exitStatus):
+              sys.stderr.write("%s failed w/ signal %d\n" % (command,os.WTERMSIG(exitStatus)))
+           else:
+              if os.WIFEXITED(exitStatus):
+                  exitStatus = os.WEXITSTATUS(exitStatus)
+              sys.stderr.write("%s failed w/ exit code %d\n" % (command,exitStatus))
+           if not streamOutput:
+              sys.stderr.write("%s\n" % ("".join(errData)))
 
-    pid,err = os.waitpid(commandPid,0)
-    commandPid = 0
+    return exitStatus,"".join(outData),"".join(errData)
 
-    try:
-       signal.signal(signal.SIGINT,sig_INT_handler)
-       signal.signal(signal.SIGHUP,sig_HUP_handler)
-       signal.signal(signal.SIGTERM,sig_TERM_handler)
-    except UnboundLocalError:
-#      happens when used in a thread
-       pass
-    except:
-       print traceback.format_exc()
 
-    if err != 0:
-        if   os.WIFSIGNALED(err):
-           sys.stderr.write("%s failed w/ signal %d\n" % (command,os.WTERMSIG(err)))
-        else:
-           if os.WIFEXITED(err):
-               err = os.WEXITSTATUS(err)
-           sys.stderr.write("%s failed w/ exit code %d\n" % (command,err))
-        if not streamOutput:
-           sys.stderr.write("%s\n" % ("".join(errData)))
+def getCommandOutput(command,
+                     streamOutput=False):
+    exitStatus,stdOut,stdErr = executeCommand(command,
+                                              streamOutput=streamOutput)
 
-    return err,"".join(outData),"".join(errData)
+    return exitStatus,stdOut,stdErr
 
 
 def getDriverNumber(driverFileName):
